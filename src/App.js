20d72447b48bdb009dd0f0a4b925af5fd166ae97
nolastemgarden <nolastemgarden@gmail.com>
2021-11-10 20:20:06 -0600

    Copy from repo nola-stem-garden-static-site

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import {
+    HashRouter as Router,
+    Route,
+    Routes
+} from "react-router-dom"
+
+// CSS & THEMING
+import './App.css';
+import theme from "./theme"
+import { ThemeProvider } from '@mui/material/styles';
+
+// PAGES
+// import Landing from "./pages/Landing"
+// import Play from "./pages/Play"
+// import Info from "./pages/Info"
+
+// MUI  components
+import { Box, CssBaseline } from '@mui/material';
+
+
+export default function App() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <ThemeProvider theme={theme}>
+                <Box id='root'
+                    sx={{
+                        // border: 'solid red 5px',
+                        bgcolor: 'primary.main',
+                        height: '100vh',
+                        width: '100vw',
+                        // overflow: 'scroll',
+                        overflow: 'hidden',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}
+                >
+                    <Router>
+                        <Routes>
+                            <Route exact path="/">
+                                {/* <Landing /> */}
+                            </Route>
+                            <Route path="/play" >
+                                {/* <Play /> */}
+                            </Route>
+                            <Route path="/info" >
+                                {/* <Info /> */}
+                            </Route>
+
+                        </Routes>
+                    </Router>
+                </Box>
+            </ThemeProvider>
+        </React.Fragment>
+    );
+}
---
+++ src/theme.js
@@ -0,0 +1,88 @@
+// in your theme file that you call `createTheme()`
+import { Theme } from '@mui/material/styles';
+
+// declare module '@mui/styles' {
+//     interface DefaultTheme extends Theme { }
+// }
+
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#669944',
+            main: '#2e6b12',
+            dark: '#004000',
+            contrastText: '#FFFFFF'
+        },
+        secondary: {
+            light: '#FFFFF',
+            main: '#669944',
+            dark: '#777777',
+            contrastText: '#FFFFFF'
+        },
+        text: {
+            primary: 'rgba(0, 0, 0, 0.87)',
+            secondary: 'rgba(0, 0, 0, 0.54)',
+            disabled: 'rgba(0, 0, 0, 0.38)',
+            hint: 'rgba(0, 0, 0, 0.38)',
+        },
+        action: {
+            disabledBackground: '#2e6b12',
+            disabledOpacity: '0.5'
+        },
+        board: {
+            light: '#2962ff',
+            main: '#0039cb',
+            dark: '#001399',
+        },
+        chip: {
+            playerOne: '#d50000',
+            playerTwo: '#ffea00',
+            unclaimed: '#b3e5fc',
+        },
+        background: '#b3e5fc',
+        white: '#FFFFFF',
+        transparent: 'rgba(0, 0, 0, 0)',
+    },
+    components: {
+        MuiPaper: {
+            styleOverrides: {  // Name of the slot
+                root: {        // Some CSS
+                    backgroundColor: '#fff',
+                    borderRadius: '50%',
+                    height: '80%',
+                    width: '80%',
+                    transform: 'translate(0, 15%)'
+
+                },
+            },
+        },
+    },
+    typography: {
+        button: {
+            fontSize: '1.0rem',
+            fontWeight: '700'
+        }
+    },
+    status: {
+
+    },
+    shape: {
+        borderRadius: 8,
+    },
+    spacing: 8,
+    transitions: {
+        easing: {
+            bounce: "cubic-bezier(0.175, 0.885, 0.32, 1.275)",
+            parabolicAcceleration: "cubic-bezier(0.5, 0.0, 1.0, 0.5)"
+        }
+    },
+    zIndex: {
+        chip: 10,
+        board: 20
+    }
+
+});
+
+export default responsiveFontSizes(theme)
