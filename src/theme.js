af8777b44a51766a83e19ad3bb6f4d5c8c5781dc
nolastemgarden <nolastemgarden@gmail.com>
2021-11-10 21:06:27 -0600

    Update name in pkg.json and fix warning in theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,5 @@
 // in your theme file that you call `createTheme()`
-import { Theme } from '@mui/material/styles';
+// import { Theme } from '@mui/material/styles';
 
 // declare module '@mui/styles' {
 //     interface DefaultTheme extends Theme { }
