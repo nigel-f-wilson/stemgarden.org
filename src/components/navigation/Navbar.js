31fd35c7cf4d0e03bb94911a3df813d5e0499a30
nolastemgarden <nolastemgarden@gmail.com>
2021-11-14 18:17:55 -0600

    Update MUI style syntax
    Remove all useStyles and classes calls and Replace with sx={{}}

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -37,144 +37,38 @@ import SchoolIcon from '@mui/icons-material/School';
 // import EcoIcon from '@mui/icons-material/EcoIcon';
 // import PhotoCameraIcon from '@mui/icons-material/PhotoCameraIcon';
 
-// import Container from '@material-ui/core/Container';
-// import Box from '@material-ui/core/Box';
-// import Grid from "@material-ui/core/Grid";
-// import Typography from '@material-ui/core/Typography';
-// import AppBar from '@material-ui/core/AppBar';
-// import Button from '@material-ui/core/Button';
-
-
-
-// import { makeStyles } from '@material-ui/core/styles';
-
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//         display: 'flex',
-//         backgroundColor: theme.palette.primary.main,
-//         zIndex: '1200',
-//     },
-//     paper: {
-//         marginRight: theme.spacing(2),
-//     },
-//     navbar: {
-//         width: '100vw',
-//         display: 'flex',
-//         flexDirection: 'row',
-//         justifyContent: 'center',
-//         backgroundColor: theme.palette.primary.main, // Matches Logo Background
-//         display: 'flex',
-//         color: theme.palette.common.white,
-//     },
-//     mobileNavbar: {
-//         height: '3.5rem',
-//     },
-//     desktopNavbar: {
-//         height: '7rem',
-//     },
-
-//     navbarContainer: {
-//         // border: 'solid red 1px',
-//         addingRight: '1.0rem',
-//         height: '100%',
-//         backgroundColor: theme.palette.primary.main, // Matches Logo Background
-//         display: 'flex',
-//         flexDirection: 'row',
-//         justifyContent: 'space-between',
-//         // alignItems: 'flex-end'
-
-//     },
-
-//     mobileLogo: {
-//         // border: 'solid red 1px',
-//         height: '100%',
-//         backgroundImage: `
-//             url(${logo})  
-//         `,
-//         backgroundPosition: 'left bottom',
-//         backgroundRepeat: 'no-repeat',
-//         backgroundSize: 'auto 95%',
-//     },
-//     mobileBrand: {
-//         height: '100%',
-
-//         // border: 'solid blue 1px',
-//         // paddingTop: '0.4rem',
-//         // paddingBottom: '0.1rem',
-//         flex: '2 0 60%',
-//         display: 'flex',
-//         // flexDirection: 'column',
-//         justifyContent: 'center',
-//         alignItems: 'center'
-//     },
-//     businessType: {
-//         lineHeight: '1.0',
-//         // paddingBottom: '0.1rem'
-//     },
-    
-
-
-//     desktopLogo: {
-//         // border: 'solid red 1px',
-//         height: '100%',
-//         paddingRight: '7rem',
-//         backgroundImage: `
-//             url(${logo})  
-//         `,
-//         backgroundPosition: 'center bottom',
-//         backgroundRepeat: 'no-repeat',
-//         backgroundSize: 'auto 95%',
-//     },
-//     desktopBrand: {
-//         // border: 'solid blue 1px',
-//         paddingBottom: '0.7rem',
-//         flex: '1 0 10%',
-//         display: 'flex',
-//         flexDirection: 'column',
-//         justifyContent: 'flex-end',
-//         fontSize: '2rem',
-//         lineHeight: '2.2rem',
-//         color: theme.palette.common.white,
-//         "text-decoration": ''
-//     },
-//     desktopMenuButtons: {
-//         // border: 'solid blue 1px',
-//         flex: '1 0 50%',
-//         display: 'flex',
-//         justifyContent: 'space-between',
-//         alignItems: 'flex-end',
-        
-//     },
-//     contactButton: {
-//         textTransform: "none",
-//     },
-    
-// }));
-
-
-
-
 export default function Navbar(props) {
     // const classes = useStyles();
     return (
         <Box 
-        // className={classes.root}  
+            sx={{ 
+                color: 'white',
+                backgroundColor: 'primary.main'
+            }}
         >
             <MobileNavbar />
             <DesktopNavbar />
-            {/* <SpacerBox /> */}
         </Box>
     )
 }
 
 function MobileNavbar(props) {
-    // const classes = useStyles();
     return (
         <Box display={{ xs: 'block', md: 'none' }} >
             <AppBar
                 // className={classes.navbar, classes.mobileNavbar}
                 position="fixed"
                 elevation={3}
+                sx={{
+                    height: '3.5rem',
+                    width: '100vw',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    backgroundColor: "primary.main", // Matches Logo Background
+                    display: 'flex',
+                    color: "white",
+                }}
             >
                 <Container
                     // className={classes.navbarContainer}
@@ -218,26 +112,64 @@ function DesktopNavbar(props) {
     return (
         <Box display={{ xs: 'none', md: 'block' }} >
             <AppBar
-                // className={classes.navbar, classes.desktopNavbar}
                 position="fixed"
                 elevation={3}
+                sx={{
+                    height: height,
+                    width: '100vw',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    backgroundColor: "primary.main", 
+                }}
             >
                 <Container
-                    // className={classes.navbarContainer}
                     maxWidth='md'
+                    sx={{
+                        backgroundColor: "primary.main", 
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        // alignItems: 'flex-end',
+                    }}
                 >
-                    <Box 
-                    // className={classes.desktopLogo} 
-
+                    <Box id="DesktopLogo"
+                        sx={{
+                            height: height,
+                            paddingRight: height,
+                            backgroundImage: `
+                                url(${logo})  
+                            `,
+                            backgroundPosition: 'center bottom',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: 'auto 95%',
+                        }}
                     />
-                    <Box 
-                    // className={classes.desktopBrand} 
-                    component={RouterLink} to='/' style={{ textDecoration: 'none' }} >
+                    <Box id="DesktopBrand"
+                        component={RouterLink} to='/' 
+                        sx={{
+                            textDecoration: 'none',
+                            paddingBottom: '0.7rem',
+                            flex: '1 0 10%',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            justifyContent: 'flex-end',
+                            fontSize: '2rem',
+                            lineHeight: '2.2rem',
+                            color: "white",
+                        }}
+                    >
                         STEM <br />Garden
                     </Box>
 
                     <Box 
                     // className={classes.desktopMenuButtons} 
+                        sx={{
+                            flex: '1 0 50%',
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'flex-end',
+                        }}
                     >
                         
                         {/* <TeachDropdown />
@@ -247,39 +179,78 @@ function DesktopNavbar(props) {
                         <ContactPageLink />
 
                     </Box>
-                     
                 </Container>
             </AppBar>
-            
         </Box>
     );
 }
 
 
 
+                // className={classes.dropdownButton}
+                sx={{
+                    color: "white",
+                    textTransform: "none",
+                    textAlign: 'center',
+                    paddingBottom: '0.5rem',
+                }}
+                        // className={classes.dropdownSurface}
+                            sx={{
+                                border: 'solid white 1px',
+                                // blurRadius: ,
+                                backgroundColor: "primary.main",
+                            }}
 
 
+                // className={classes.dropdownButton}
+                sx={{
+                    color: "white",
+                    textTransform: "none",
+                    textAlign: 'center',
+                    paddingBottom: '0.5rem',
+                }}
 
+                        // className={classes.dropdownSurface}  
+                            sx={{
+                                border: 'solid white 0.1px',
+                                backgroundColor: "primary.main",
+                            }}
 
 
 
 function ContactPageLink() {
     // const classes = useStyles();
     
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
+                // className={classes.listItem}
+                    // classes={{ root: classes.icon }}
     return (
         <Box 
         // className={classes.desktopMenuButton}
         >
             <Button
-                // className={classes.contactButton}
                 variant="text"
+                sx={{ textTransform: "none" }}
             >
-                <Typography variant="h5">
+                <Typography color="white"  variant="h5">
                     Contact
                 </Typography>
-
             </Button>
-            
         </Box>
     );
 }
