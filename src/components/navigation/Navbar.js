6572b0fb46999cd9ce77bc2448c1005fa4632f8d
nolastemgarden <nolastemgarden@gmail.com>
2021-11-14 18:19:25 -0600

    Copy in Verbose GardenNavList

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -440,12 +440,81 @@ function TeachingServicesList(props) {
     )
 }
 
+
+
+function GardenNavList(props) {
+    const handleClose = props.handleClose
+
+    return (
+        <React.Fragment>
+            <ListItem
+                key={'our_story'}
                 // className={classes.listItem}
+                button
+                onClick={handleClose}
+                component={RouterLink}
+                to={{
+                    pathname: '/garden',
+                    state: {
+                        selectedTab: 'our_story',
+                    }
+                }}
+            >
+                <Icon className={"fas fa-leaf fa-2x"}
                     // classes={{ root: classes.icon }}
+                />
+                <ListItemText
+                    primary={'Our Story'}
+                    primaryTypographyProps={{ variant: 'h5' }}
+                />
+            </ListItem>
+
+            <ListItem
+                key={'whats_growing'}
                 // className={classes.listItem}
+                button
+                onClick={handleClose}
+                component={RouterLink}
+                to={{
+                    pathname: '/garden',
+                    state: {
+                        selectedTab: 'whats_growing',
+                    }
+                }}
+            >
+                <Icon className="fas fa-camera fa-2x"
                     // classes={{ root: classes.icon }}
+                />
+                <ListItemText
+                    primary={"What's Growing"}
+                    primaryTypographyProps={{ variant: 'h5' }}
+                />
+            </ListItem>
+
+            <ListItem
+                key={'for_sale'}
                 // className={classes.listItem}
+                button
+                onClick={handleClose}
+                component={RouterLink}
+                to='/produce_for_sale'
+            >
+                <Icon className="fas fa-carrot fa-2x"
                     // classes={{ root: classes.icon }}
+                />
+                <ListItemText
+                    primary={'Produce for Sale'}
+                    primaryTypographyProps={{ variant: 'h5' }}
+                />
+            </ListItem>
+
+
+        </React.Fragment>
+    );
+}
+
+
+
     return (
         <Box 
         // className={classes.desktopMenuButton}
