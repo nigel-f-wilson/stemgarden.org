d1dd280705f31a4f5ed7caa988134f906f7d64ab
nolastemgarden <nolastemgarden@gmail.com>
2022-03-04 13:35:00 -0600

    Extract Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
 import React from 'react'
-import {
-    Route,
-    Routes
-} from "react-router-dom"
+import Router from './Router'
 
 // CSS & THEMING
-import './App.css';
 import theme from "./theme"
 import { ThemeProvider } from '@mui/material/styles';
-
-// PAGES
-import { LandingPage } from "./pages/LandingPage"
-// import { MathGamesPage } from "./pages/MathGamesPage"
-// import { ContactPage } from "./pages/ContactPage"
-// import { TeachingServicesPage } from "./pages/TeachingServicesPage"
-
 
 // MUI  components
 import { Box, CssBaseline } from '@mui/material';
@@ -27,22 +16,13 @@ export default function App() {
             <ThemeProvider theme={theme}>
                 <Box id='root'
                     sx={{
-                        // border: 'solid red 5px',
                         bgcolor: 'primary.main',
                         height: '100vh',
                         width: '100vw',
-                        // overflow: 'scroll',
                         overflow: 'hidden',
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                        justifyContent: 'center',
                     }}
                 >
-                    <Routes>
-                        <Route exact path="/" element={<LandingPage />} />
-                        <Route path="/math_games" element={<LandingPage />} />
-                    </Routes>
+                    <Router />
                 </Box>
             </ThemeProvider>
         </React.Fragment>
