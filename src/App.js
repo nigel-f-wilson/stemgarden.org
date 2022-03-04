1aca66fd001b120f0d6f9e166ff340e37691c624
nolastemgarden <nolastemgarden@gmail.com>
2022-03-04 17:14:23 -0600

    Add Navbar to App, above router

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import Router from './Router'
 
+import { useScreenWidth } from "./hooks"
+
+
 // CSS & THEMING
 import theme from "./theme"
 import { ThemeProvider } from '@mui/material/styles';
 
 // MUI  components
 import { Box, CssBaseline } from '@mui/material';
+import Navbar from './components/navigation/Navbar';
+
 
 
 export default function App() {
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            <ThemeProvider theme={theme}>
-                <Box id='root'
-                    sx={{
-                        bgcolor: 'primary.main',
-                        height: '100vh',
-                        width: '100vw',
-                        overflow: 'hidden',
-                    }}
-                >
-                    <Router />
-                </Box>
-            </ThemeProvider>
-        </React.Fragment>
-    );
+  const screenWidth = useScreenWidth()
+  const smallScreen = screenWidth < 600
+
+  const defaultBackground = {
+    bgcolor: 'primary.main',
+    height: '100vh',
+    width: '100vw',
+    overflow: 'hidden',
+  }
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Box id='root' sx={defaultBackground} >
+          <Navbar />
+          <Router />
+        </Box>
+      </ThemeProvider>
+    </React.Fragment>
+  );
 }
