83bb47a1a4032571bef5397716d5774001d5c472
nolastemgarden <nolastemgarden@gmail.com>
2022-03-09 16:05:50 -0600

    Add testing route

--- src/Router.js
+++ src/Router.js
@@ -8,18 +8,21 @@ export default function Router() {
   return (
     <Routes>
       <Route exact path="/" element={<Landing />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} >
+      <Route path="about" element={<About />} />
+
+        <Route path="tutoring-services" element={<Services />} />   {/* <Route path="tutoring" element={<Services activeTab="tutoring" />} /> */}
+
+      <Route path="services" element={<Services />} >
         <Route path="tutoring" element={<Services />} />   {/* <Route path="tutoring" element={<Services activeTab="tutoring" />} /> */}
         <Route path="enrichment" element={<Services />} />
         <Route path="professional-development" element={<Services />} />
         <Route path="chess-clubs" element={<ChessClubs />} />
       </Route>
-      <Route path="/math-games" element={<MathGames />} >
+      <Route path="math-games" element={<MathGames />} >
         <Route path="the-15-game" element={<Services />} />
         <Route path="connect-four" element={<Services />} />
       </Route>
-      <Route path="/contact" element={<Contact />} />
+      <Route path="contact" element={<Contact />} />
 
     </Routes>
   );
