c1c157a1016217ae6d343da9995058e4a679e1a4
nolastemgarden <nolastemgarden@gmail.com>
2022-03-13 17:13:01 -0500

    Add code splitting with React.Lazy imports

--- src/Router.js
+++ src/Router.js
@@ -1,29 +1,48 @@
-import React from 'react'
+import React, { lazy, Suspense  } from 'react'
+
 import { Route, Routes } from "react-router-dom"
 
 // PAGES
-import { Landing, About, Contact, MathGames, ChessClubs, Services } from "./pages/index"
+// import { About, Contact, MathGames, ChessClubs, Services } from "./pages/index"
+import Landing from './pages/Landing'
+const About = lazy(() => import('./pages/About'))
+const Contact = lazy(() => import('./pages/Contact'))
+const MathGames = lazy(() => import('./pages/MathGames'))
+const ChessClubs = lazy(() => import('./pages/ChessClubs'))
+const Services = lazy(() => import('./pages/Services'))
+
+const renderLoader = () => <p>Loading</p>;
 
 export default function Router() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Landing />} />
-      <Route path="about" element={<About />} />
+    <Suspense fallback={renderLoader()}>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
 
-        <Route path="tutoring-services" element={<Services />} />   {/* <Route path="tutoring" element={<Services activeTab="tutoring" />} /> */}
-
-      <Route path="services" element={<Services />} >
-        <Route path="tutoring" element={<Services />} />   {/* <Route path="tutoring" element={<Services activeTab="tutoring" />} /> */}
-        <Route path="enrichment" element={<Services />} />
-        <Route path="professional-development" element={<Services />} />
-        <Route path="chess-clubs" element={<ChessClubs />} />
-      </Route>
-      <Route path="math-games" element={<MathGames />} >
-        <Route path="the-15-game" element={<Services />} />
-        <Route path="connect-four" element={<Services />} />
-      </Route>
-      <Route path="contact" element={<Contact />} />
-
+        <Route path="services" element={<Services />} >
+          <Route path="tutoring" element={<Services />} />   {/* <Route path="tutoring" element={<Services activeTab="tutoring" />} /> */}
+          <Route path="enrichment" element={<Services />} />
+          <Route path="professional-development" element={<Services />} />
+          <Route path="chess-clubs" element={<ChessClubs />} />
+        </Route>
+        <Route path="math-games" element={<MathGames />} >
+          <Route path="the-15-game" element={<Services />} />
+          <Route path="connect-four" element={<Services />} />
+        </Route>
+        {/* <Route path="garden" element={<Garden />} >
+          <Route path="what_we_grow" element={<Services />} />  
+          <Route path="our_story" element={<Services />} />
+          <Route path="buy_our_fruit" element={<Services />} />
+          <Route path="get_involved" element={<ChessClubs />} />
+        </Route>
+        <Route path="people" element={<People />} >
+          <Route path="nigel" element={<Nigel />} />  
+          <Route path="olive" element={<Olive />} />
+          <Route path="you" element={<Join />} />
+        </Route> */}
+        <Route path="contact" element={<Contact />} />
     </Routes>
+
+      </Suspense>
   );
 }
