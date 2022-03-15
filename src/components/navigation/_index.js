a1811b39183bf4eed5b106a7cde7e44936120b8e
nolastemgarden <nolastemgarden@gmail.com>
2022-03-15 17:23:50 -0500

    Create _index.js

---
+++ src/components/navigation/_index.js
@@ -0,0 +1,7 @@
+export { default as Logo } from "./Logo";
+export { default as DesktopNavbar } from "./DesktopNavbar";
+export { default as MobileNavbar } from "./MobileNavbar";
+export { default as MobileMenu } from "./MobileMenu";
+export { default as ServicesList } from "./ServicesList";
+// export { default as AboutList } from "./AboutList";
+
