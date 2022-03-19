e7a1262a0d953565366c7f7c30d13e5d4362c015
nolastemgarden <nolastemgarden@gmail.com>
2022-03-19 15:57:24 -0500

    Update TicTacToeCoach.js

--- src/math-games/tic-tac-toe/TicTacToeCoach.js
+++ src/math-games/tic-tac-toe/TicTacToeCoach.js
@@ -1,17 +1,24 @@
 import React from 'react'
 
-// import { 
-//   Container, 
-//   Box, 
-//   Typography } from '@mui/material';
-// import { common } from '@mui/material/colors';
+import { 
+  Container, 
+  Box, 
+  Typography } from '@mui/material';
+import theme from '../../theme';
+
 
 export default function TicTacToeCoach(props) {
   
   return (
-    <React.Fragment >
-    
-    </React.Fragment>
+    <Box id="tic tac toe"
+      width='100vw' 
+      height='100vh'
+      bgColor={theme.palette.common.black}
+      zIndex={9000}
+    >
+      
+
+    </Box>
   )
 }
 
