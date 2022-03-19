8de135193b7d7d1aee4e3d2601c77bb6089e6234
nolastemgarden <nolastemgarden@gmail.com>
2022-03-19 15:12:37 -0500

    Create TicTacToeCoach.js
    stub

---
+++ src/math-games/tic-tac-toe/TicTacToeCoach.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// import { 
+//   Container, 
+//   Box, 
+//   Typography } from '@mui/material';
+// import { common } from '@mui/material/colors';
+
+export default function TicTacToeCoach(props) {
+  
+  return (
+    <React.Fragment >
+    
+    </React.Fragment>
+  )
+}
+
