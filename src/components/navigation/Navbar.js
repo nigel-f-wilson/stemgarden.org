9fe379125d7622e419b648ac8e6e51e6a6267cb9
nolastemgarden <nolastemgarden@gmail.com>
2022-03-19 15:12:08 -0500

    Update Navbar.js

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -2,23 +2,18 @@ import React from 'react';
 import { useScreenWidth } from "../../hooks";
 
 import { MobileNavbar, DesktopNavbar } from "./_index";
-
-import { Container, Box } from "@mui/material"
+import { Box } from "@mui/material"
 
 export default function Navbar(props) {
     const screenWidth = useScreenWidth()
     return (
       <Box 
+        children={(screenWidth >= 600) ? <DesktopNavbar /> : <MobileNavbar />  } 
         sx={{ 
           width: '100vw',
           backgroundColor: 'primary.main'
         }}
-      >
-        {/* <Container maxWidth='md' id="navbar-container" > */}
-          {/* <MobileNavbar /> */}
-          {(screenWidth >= 600) ? <DesktopNavbar /> : <MobileNavbar />  }  
-        {/* </Container> */}
-      </Box>
+      />
     )
 }
 
