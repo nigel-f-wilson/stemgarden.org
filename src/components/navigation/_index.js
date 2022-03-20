e99b2edc9928480af41d33c8cf1df5a98af04960
nolastemgarden <nolastemgarden@gmail.com>
2022-03-20 13:24:25 -0500

    Update _index.js

--- src/components/navigation/_index.js
+++ src/components/navigation/_index.js
@@ -1,5 +1,5 @@
 export { default as Logo } from "./Logo";
 export { default as DesktopNavbar } from "./DesktopNavbar";
 export { default as MobileNavbar } from "./MobileNavbar";
-export { default as MobileMenu } from "./MobileMenu";
+export { default as MobileMenu } from "./MobileMenuDrawer";
 
