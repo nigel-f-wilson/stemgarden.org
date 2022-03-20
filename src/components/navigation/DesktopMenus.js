a1fd7c0b34b17f668ea504b2cc2c4b4879674e6e
nolastemgarden <nolastemgarden@gmail.com>
2022-03-20 13:24:31 -0500

    Update DesktopMenus.js

--- src/components/navigation/DesktopMenus.js
+++ src/components/navigation/DesktopMenus.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import { Link as RouterLink } from "react-router-dom";
 
-// MY COMPONENTS
+import DesktopPopper from "./DesktopPopper";
 
-import { common } from '@mui/material/colors';
 import {  
     Box, 
-    Typography,
-    Button,
-    ClickAwayListener,
-    Grow,
-    Paper,
-    Popper,
-    List,
-    MenuList,
-    ListItem,
-    ListItemText,
 } from "@mui/material"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -26,7 +14,7 @@ export default function DesktopMenus() {
     const [openDropdown, setOpenDropdown] = React.useState("none")   // "none", "about", "services", "resources"
 
     const setDropdownState = (newState) => {
-      // console.log(`setOpenDropdown state to: ${newState}`)
+      console.log(`setOpenDropdown state to: ${newState}`)
       setOpenDropdown(newState)
     }
  
@@ -40,129 +28,25 @@ export default function DesktopMenus() {
           justifyContent='space-between'
           alignItems='flex-end'
         >
-            <DesktopMenu
-              name="about"
-              children={aboutListData}
-              open={openDropdown === "about"}
-              setDropdownState={setDropdownState}
-            />
-            <DesktopMenu
-              name="services"
-              children={servicesListData}
-              open={openDropdown === "services"}
-              setDropdownState={setDropdownState}
-            />
-            <DesktopMenu
-              name="resources"
-              children={resourcesListData}
-              open={openDropdown === "resources"}
-              setDropdownState={setDropdownState}
-            />
-            {/* <ContactPageLink />  */}
-        </Box>
-    )
-}
-
-function DesktopMenu(props) {
-    let { name, children, open, setDropdownState } = props
-    
-    const anchorRef = React.useRef(null);
-
-    const handleClickAway = (event) => {
-      if (anchorRef.current && anchorRef.current.contains(event.target)) {
-          return; // do nothing, not a click "away"
-      }
-      setDropdownState("none")
-    }
-    const handleToggle = () => {
-      if (open) { setDropdownState("none") }
-      else { setDropdownState(name)}
-    }
-    const handleClose = () => {
-      setDropdownState("none") 
-    }
-    const capitalize = (string) => {
-      return  string[0].toUpperCase() + string.slice(1).toLowerCase()
-    }
-
-    return (
-        <Box border='solid red 1px' >
-            <Button
-                onClick={handleToggle}
-                variant="text"
-                sx={{
-                    color: "white",
-                    textTransform: "none",
-                    textAlign: 'center',
-                    paddingBottom: '0.5rem',
-                }}
-                ref={anchorRef}
-                aria-controls={open ? 'menu-list-grow' : undefined}
-                aria-haspopup="true"
-            >
-                <Typography variant="h5" children={capitalize(name)} />
-            </Button>
-            <Popper
-                open={open}
-                anchorEl={anchorRef.current}
-                role="menu" // What values can this take?
-                transition
-                disablePortal
-            >
-                {({ TransitionProps }) => (
-                    <Grow
-                        {...TransitionProps}
-                        style={{ transformOrigin: 'center top' }}
-                    >
-                        <Paper
-                            onClick={handleClose}
-                            elevation={3}
-                            sx={{
-                                border: 'solid white 1px',
-                                backgroundColor: "primary.main",
-                            }}
-                        >
-                            <ClickAwayListener onClickAway={handleClickAway}>
-                                {children}
-                                <MenuList autoFocusItem={open} >
-                                    <List>
-                                        {
-                                            children.map((item, index) => {
-                                                const { label, linkTo, icon } = item
-                                                return (
-                                                    <ListItem
-                                                        key={index}
-                                                        button
-                                                        component={RouterLink}
-                                                        to={linkTo}
-                                                    >
-                                                        <FontAwesomeIcon 
-                                                            icon={item.icon} 
-                                                            size="2x" 
-                                                            transform="shrink-2" 
-                                                            color={common.white}
-                                                        />
-                                                        <ListItemText
-                                                            primary={item.label}
-                                                            primaryTypographyProps={{ 
-                                                                variant: 'h5', 
-                                                                color: common.white
-                                                            }}
-                                                            sx={{
-                                                                paddingLeft: "1rem",
-                                                            }}
-                                                        />
-                                                    </ListItem>
-                                                )
-                                            })
-                                        }
-                                    </List>
-                                </MenuList>
-                            </ClickAwayListener>
-                        </Paper>
-                    </Grow>
-                )}
-            </Popper>
+          <DesktopPopper
+            name="about"
+            listData={aboutListData}
+            open={openDropdown === "about"}
+            setDropdownState={setDropdownState}
+          />
+          <DesktopPopper
+            name="services"
+            listData={servicesListData}
+            open={openDropdown === "services"}
+            setDropdownState={setDropdownState}
+          />
+          <DesktopPopper
+            name="resources"
+            listData={resourcesListData}
+            open={openDropdown === "resources"}
+            setDropdownState={setDropdownState}
+          />
+          {/* <ContactPageLink />  */}
         </Box>
     )
 }
