16c5d4e2502cae41453a22baa71b012d0835a912
nolastemgarden <nolastemgarden@gmail.com>
2022-03-20 12:43:49 -0500

    Update DesktopNavbar.js

--- src/components/navigation/DesktopNavbar.js
+++ src/components/navigation/DesktopNavbar.js
@@ -2,26 +2,13 @@ import React from 'react';
 import { Link as RouterLink } from "react-router-dom";
 
 // MY COMPONENTS
-// import FlexRow from "../../components/FlexRow";
-import { aboutList, servicesList } from "./menuItems";
-// import ServicesList from "./ServicesList";
+import DesktopMenus from "./DesktopMenus";
 import Logo from "./Logo";
 
-
-import { common } from '@mui/material/colors';
-import { Container, 
+import { 
     Box, 
     Typography,
     AppBar,
-    Button,
-    ClickAwayListener,
-    Grow,
-    Paper,
-    Popper,
-    List,
-    MenuList,
-    ListItem,
-    ListItemText,
 } from "@mui/material"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -70,152 +57,6 @@ function DesktopBrand() {
             >
                 STEM<br />Garden
             </Typography>
-        </Box>
-    )
-}
-
-function DesktopMenus() {
-    const [openDropdown, setOpenDropdown] = React.useState("none")   // "none", "about", "services", "resources"
-
-    const setDropdownState = (newState) => {
-      // console.log(`setOpenDropdown state to: ${newState}`)
-      setOpenDropdown(newState)
-    }
- 
-    return (
-        <Box 
-          flexGrow='1'
-          pl={24}
-          pr={4}
-          display='flex'
-          flexDirection='row'
-          justifyContent='space-between'
-          alignItems='flex-end'
-        >
-            <DesktopMenu
-                name="about"
-                items={aboutList}
-                open={openDropdown === "about"}
-                setDropdownState={setDropdownState}
-            />
-            <DesktopMenu
-                name="services"
-                items={servicesList}
-                open={openDropdown === "services"}
-                setDropdownState={setDropdownState}
-            />
-            <DesktopMenu
-                name="resources"
-                
-            />
-            {/* <ContactPageLink />  */}
-        </Box>
-    )
-}
-
-function DesktopMenu(props) {
-    let { name, items, open, setDropdownState } = props
-    
-    const anchorRef = React.useRef(null);
-
-    const handleClickAway = (event) => {
-      if (anchorRef.current && anchorRef.current.contains(event.target)) {
-          return; // do nothing, not a click "away"
-      }
-      setDropdownState("none")
-    }
-    const handleToggle = () => {
-      if (open) { setDropdownState("none") }
-      else { setDropdownState(name)}
-    }
-    const handleClose = () => {
-      setDropdownState("none") 
-    }
-    const capitalize = (string) => {
-      return  string[0].toUpperCase() + string.slice(1).toLowerCase()
-    }
-
-    return (
-        <Box border='solid red 1px' >
-            <Button
-                onClick={handleToggle}
-                variant="text"
-                sx={{
-                    color: "white",
-                    textTransform: "none",
-                    textAlign: 'center',
-                    paddingBottom: '0.5rem',
-                }}
-                ref={anchorRef}
-                aria-controls={open ? 'menu-list-grow' : undefined}
-                aria-haspopup="true"
-            >
-                <Typography variant="h5" children={capitalize(name)} />
-            </Button>
-            <Popper
-                open={open}
-                anchorEl={anchorRef.current}
-                role="menu" // What values can this take?
-                transition
-                disablePortal
-            >
-                {({ TransitionProps }) => (
-                    <Grow
-                        {...TransitionProps}
-                        style={{ transformOrigin: 'center top' }}
-                    >
-                        <Paper
-                            onClick={handleClose}
-                            elevation={3}
-                            sx={{
-                                border: 'solid white 1px',
-                                backgroundColor: "primary.main",
-                            }}
-                        >
-                            <ClickAwayListener onClickAway={handleClickAway}>
-                                <MenuList autoFocusItem={open} >
-                                    <List>
-                                        {
-                                            items.map((item, index) => {
-                                                return (
-                                                    <ListItem
-                                                        key={index}
-                                                        button
-                                                        component={RouterLink}
-                                                        to={{
-                                                            pathname: item.pathname,
-                                                            state: {
-                                                                selectedTab: item.activeTab,
-                                                            }
-                                                        }}
-                                                    >
-                                                        <FontAwesomeIcon 
-                                                            icon={item.icon} 
-                                                            size="2x" 
-                                                            transform="shrink-2" 
-                                                            color={common.white}
-                                                        />
-                                                        <ListItemText
-                                                            primary={item.label}
-                                                            primaryTypographyProps={{ 
-                                                                variant: 'h5', 
-                                                                color: common.white
-                                                            }}
-                                                            sx={{
-                                                                paddingLeft: "1rem",
-                                                            }}
-                                                        />
-                                                    </ListItem>
-                                                )
-                                            })
-                                        }
-                                    </List>
-                                </MenuList>
-                            </ClickAwayListener>
-                        </Paper>
-                    </Grow>
-                )}
-            </Popper>
         </Box>
     )
 }
