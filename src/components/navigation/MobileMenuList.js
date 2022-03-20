effc9e0e99070d206fb53d2b434f8a4c79e95737
nolastemgarden <nolastemgarden@gmail.com>
2022-03-20 12:43:35 -0500

    Create MobileMenuList.js

---
+++ src/components/navigation/MobileMenuList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link as RouterLink } from "react-router-dom";
+import { useTheme } from '@mui/styles';
+
+// MATERIAL-UI COMPONENTS
+import { 
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+export default function MobileMenuList(props) {
+  const { listData, handleClose } = props
+  // const { handleClose } = props
+  const theme = useTheme()
+    
+  return (
+    <List >
+      {listData.map((item, index) => {
+          const { label, linkTo, icon } = item
+          return (
+            <ListItem
+              key={index}
+              button
+              onClick={handleClose}
+              component={RouterLink}
+              to={linkTo}
+            >
+              <Box  width={24} pr={2} display='flex' justifyContent='center' >
+                <FontAwesomeIcon
+                  icon={icon} 
+                  color='white'
+                  size='lg'
+                />
+              </Box>
+              <ListItemText
+                  primary={label}
+                  primaryTypographyProps={{ 
+                    variant: 'h5',
+                    paddingLeft: 1 
+                  }}
+              />
+            </ListItem>
+          )
+        })}
+    </List>
+  )
+}
