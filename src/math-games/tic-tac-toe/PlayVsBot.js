c427f8f6be2583b22a06883a505fad0cd7e37bdb
nolastemgarden <nolastemgarden@gmail.com>
2022-03-20 17:16:38 -0500

    Create PlayVsBot.js

---
+++ src/math-games/tic-tac-toe/PlayVsBot.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { 
+  Container, 
+  Box, 
+  Typography } from '@mui/material';
+import theme from '../../theme';
+
+
+export default function TicTacToe(props) {
+  
+  return (
+    <Box 
+      border='solid red 2px'
+      width='100vw' 
+      height='calc(100vh - 96px)'
+      overflow='hidden'
+      bgcolor={theme.palette.common.black}
+      color={theme.palette.common.white}
+
+      zIndex={9000}
+    >
+    <Container maxWidth='md'>
+      <Typography variant='h1'  > Play Vs Bot Page</Typography>
+
+    </Container>
+
+    </Box>
+  )
+}
+
