186001967bba5f096ff9bec1b3d2396e1f1b5f58
nolastemgarden <nolastemgarden@gmail.com>
2022-03-30 21:23:43 -0500

    Update Square.js

--- src/math-games/tic-tac-toe/components/Square.js
+++ src/math-games/tic-tac-toe/components/Square.js
@@ -5,9 +5,18 @@ import React from 'react';
 // My Components
 
 // MUI  components
-import { Paper } from '@mui/material';
+import { Box, Paper, Typography } from '@mui/material';
 
-import { ClearIcon, RadioButtonUncheckedIcon } from '@mui/icons-material';
+import ClearIcon from '@mui/icons-material/Clear';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes, faO, faXmark, faXmarkLarge } from '@fortawesome/free-solid-svg-icons';
+
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
 
 // Custom Styling
 // import { makeStyles } from '@material-ui/core/styles';
@@ -43,56 +52,70 @@ import { ClearIcon, RadioButtonUncheckedIcon } from '@mui/icons-material';
 //     },
 // }));
 
-// export default function Square(props) {
-//     // const classes = useStyles();
+export default function Square(props) {
+  const { icon, color, number, handleBoardClick } = props
 
-//     let squareIcon;
-//     switch (props.icon) {
-//         case 'x':
-//             squareIcon = <ClearIcon className={classes.iconX} />
-//             break;
-//         case 'o':
-//             squareIcon = <RadioButtonUncheckedIcon className={classes.iconO} />
-//             break;
-//         case '_':
-//             // squareIcon = <Typography variant='h3' color='textSecondary' >{props.number}</Typography> // 
-//             break;
-//         default:
-//             console.error("Square passed symbol not 'x' 'o' or '_'");
-//     }
+    let squareIcon;
+    switch (icon) {
+        case 'x':
+            squareIcon = <FontAwesomeIcon icon={faTimes} size='6x' />
+            break;
+        case 'o':
+            squareIcon = <FontAwesomeIcon icon={faO} />
+            break;
+        case '_':
+            squareIcon = <Typography variant='h3' color='textSecondary' >{props.number}</Typography> // 
+            break;
+        default:
+            console.error("Square passed symbol not 'x' 'o' or '_'");
+    }
 
-//     let squareColorClassName;
-//     switch (props.color) {
-//         case 'claimed':
-//             squareColorClassName = `${classes.square} ${classes.claimed} `
-//             break;
-//         case 'unclaimed':
-//             squareColorClassName = `${classes.square} ${classes.unclaimed} `
-//             break;
-//         case 'noColor':
-//             squareColorClassName = `${classes.square} ${classes.noColor} `
-//             break;
-//         case 'draw':
-//             squareColorClassName = `${classes.square} ${classes.draw} `
-//             break;
-//         case 'win':
-//             squareColorClassName = `${classes.square} ${classes.win} `
-//             break;
-//         case 'lose':
-//             squareColorClassName = `${classes.square} ${classes.lose} `
-//             break;
-//         default:
-//             squareColorClassName = `${classes.square} `
-//     }
+    // let squareColorClassName;
+    // switch (props.color) {
+    //   case 'claimed':
+    //       squareColorClassName = `${classes.square} ${classes.claimed} `
+    //       break;
+    //   case 'unclaimed':
+    //       squareColorClassName = `${classes.square} ${classes.unclaimed} `
+    //       break;
+    //   case 'noColor':
+    //       squareColorClassName = `${classes.square} ${classes.noColor} `
+    //       break;
+    //   case 'draw':
+    //       squareColorClassName = `${classes.square} ${classes.draw} `
+    //       break;
+    //   case 'win':
+    //       squareColorClassName = `${classes.square} ${classes.win} `
+    //       break;
+    //   case 'lose':
+    //       squareColorClassName = `${classes.square} ${classes.lose} `
+    //       break;
+    //   default:
+    //       squareColorClassName = `${classes.square} `
+    // }
 
-//     return (
-//         <Paper
-//             number={props.number}
-//             elevation={4}
-//             className={squareColorClassName}
-//             onClick={() => props.handleBoardClick(props.number.toString())}
-//         >
-//             {squareIcon}
-//         </Paper>
-//     )
-// }
+    return (
+      <Box
+        p={1}
+        width='33.3%'
+        height='100%'
+        display='flex'
+      >
+        <Paper
+          // className={squareColorClassName}
+
+          number={number}
+          onClick={() => handleBoardClick(number.toString())}
+          sx={{
+            width: '100%',
+            backgroundColor: 'common.white',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          {squareIcon}
+      </Paper>
+      </Box>
+    )
+}
