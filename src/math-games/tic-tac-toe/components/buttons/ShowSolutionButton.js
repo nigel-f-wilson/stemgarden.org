4f87455397f010d9a69d3b42ee6268d3f7550c2c
nolastemgarden <nolastemgarden@gmail.com>
2022-04-03 17:35:49 -0500

    Update ShowSolutionButton.js

--- src/math-games/tic-tac-toe/components/buttons/ShowSolutionButton.js
+++ src/math-games/tic-tac-toe/components/buttons/ShowSolutionButton.js
@@ -1,40 +1,26 @@
 import React from 'react';
 
-// My Components
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 
-// MUI Components
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
 
-// MUI Icons
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-
-// Custom Styling
-import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles((theme) => ({
-    button: {
-        backgroundColor: theme.palette.primary.main,
-        width: '100%',
-        height: '2.1rem',
-        fontSize: '1rem',
-    },
-}));
-
-export default function ShowHintsButton(props) {
-    const classes = useStyles();
+export default function ShowSolutionButton(props) {
+    const { showSolution, toggleShowSolution } = props
 
     return (
-        <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={() => props.toggleShowHints()}
-        >
-            <Box mr={2} display="flex" alignContent="center" >
-                <HelpOutlineIcon />
-            </Box>
-            {props.showHints ? "Hide Hints" : "Show Hints"}
-        </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => toggleShowSolution()}
+        sx={{  width: '100%', }}
+      >
+        <Box mr={2} display="flex" alignContent="center" >
+          <FontAwesomeIcon icon={faQuestion} size='lg' />
+        </Box>
+        {showSolution ? "Hide Solution" : "Show Solution"}
+      </Button>
     )
 }
     
