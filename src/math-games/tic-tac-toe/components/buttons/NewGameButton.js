3a89fbeb0e5f2744e9f030e3652de66a42c6f3d3
nolastemgarden <nolastemgarden@gmail.com>
2022-04-03 17:35:44 -0500

    Update NewGameButton.js

--- src/math-games/tic-tac-toe/components/buttons/NewGameButton.js
+++ src/math-games/tic-tac-toe/components/buttons/NewGameButton.js
@@ -6,38 +6,31 @@ import React from 'react';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 
-// MUI Icons
-import ReplayIcon from '@material-ui/icons/Replay';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
-
-// Custom Styling
-import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles((theme) => ({
-    button: {
-        backgroundColor: theme.palette.primary.main,
-        width: '100%',
-        height: '2.1rem',
-        fontSize: '1rem',
-    },
-}));
 
 export default function NewGameButton(props) {
-    const classes = useStyles();
-
-    return (
-        <Button
-            className={classes.button}
-            variant="contained"
-            color="primary"
-            onClick={() => props.handleNewGameClick()}
-            disabled={!props.gameOver}
-        >
-            <Box mr={2} display="flex" alignContent="center" >
-                <ReplayIcon />
-            </Box>
-            New&nbsp;Game
-        </Button>
-    )
+  const { gameOver, disabled } = props
+  return (
+    <Button
+      className={classes.button}
+      variant="contained"
+      color="primary"
+      onClick={() => props.handleNewGameClick()}
+      disabled={!gameOver}
+      sx={{ 
+        width: '100%',
+        height: '2.1rem',
+        fontSize: '1rem', 
+      }}
+    >
+      <Box mr={2} display="flex" alignContent="center" >
+        <FontAwesomeIcon icon={faRotateLeft} size='lg' />
+      </Box>
+      New&nbsp;Game
+    </Button>
+  )
 }
 
 
