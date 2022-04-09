c4a8621a6a2de69c6c3872e11ecbf184385ecae2
nolastemgarden <nolastemgarden@gmail.com>
2022-04-09 11:35:50 -0500

    Begin adapting TTT board for 15G

---
+++ src/math-games/fifteen-game/components/Board.js
@@ -0,0 +1,131 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import { status, 
+  moveListStringToArray, 
+  numbersInWin, 
+  availableNumbers, 
+} from "../logic/gameLogic";
+
+// My Components
+import Square from "./Square";
+
+// MUI  
+import { useTheme } from "@mui/system";
+import { Box } from '@mui/material';
+
+export default function Board(props) {
+  const { moveList, outcomeMap, showSolution, handleSquareClick } = props
+  const theme = useTheme()
+
+  const [height, setHeight] = useState(100);
+  const boardRef = useRef();
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((event) => {
+      setHeight(event[0].contentBoxSize[0].blockSize);
+    });
+    if (boardRef) {
+      resizeObserver.observe(boardRef.current);
+    }
+  }, [boardRef]);
+    
+
+  const icons = getIcons(moveList)
+  const colors = getColors(moveList, showSolution, outcomeMap)
+  
+  const cardNumbers = [1,2,3,4,5,6,7,8,9]
+  let cards = []
+  cardNumbers.forEach(num => {
+    let newSquare =
+      <Square
+        key={num}
+        number={num}
+        icon={icons[num]}
+        color={colors[num]}
+        handleSquareClick={handleSquareClick}
+      />
+    cards.push(newSquare);
+  })
+
+  return (
+    <Box ref={boardRef}
+      id='board height container'
+      height={theme.breakpoints.values.sm}
+      maxHeight='50%'
+      width='100%'
+      display='flex'
+      justifyContent='center'
+      my={1}
+    >  
+      <Box id='board width container'
+        height='100%'
+        width='100%'
+        border='solid red 2px'
+      >  
+        <Box id='row1' 
+          children={cards.slice(0,5)}
+          width='100%'
+          height='50%' 
+          display='flex'
+        />
+        <Box id='row2' 
+          children={cards.slice(5,9)}
+          height='50%' 
+          display='flex'
+        />
+      </Box>
+    </Box>
+  )
+}
+
+function getColors(ml, showSolution, outcomeMap) {
+  let gameStatus = status(ml)
+  if (gameStatus === "xWins" || gameStatus === "oWins") {
+    return highlightWins(ml)
+  }
+  else if (showSolution === true) {
+    return getBoardHints(ml, outcomeMap)
+  }
+  else {
+    return Array(10).fill('noColor')
+  }
+}
+
+function getIcons(ml) {
+  let data = Array(10).fill('_');  // Start with an array representing a board of NINE empty squares
+  let mlArray = moveListStringToArray(ml)
+  mlArray.forEach((squareId, turn) => {
+      data[squareId] = (turn % 2 === 0) ? 'x' : 'o'
+  })
+  return data;  
+}
+
+function highlightWins(ml) {
+  let colors = Array(10).fill('noColor')
+  numbersInWin(ml).forEach(num => colors[num] = 'win')
+  return colors
+}
+
+function getBoardHints(ml, outcomeMap) {
+  let colors = Array(10).fill('noColor')
+  availableNumbers(ml).forEach(num => {
+    let outcome = outcomeMap.get(ml + num.toString())
+    colors[num] = getHintColor(outcome, ml)
+  })
+  return colors
+}
+
+function getHintColor(outcome, ml) {
+  let gameStatus = status(ml)
+  if (outcome === "draw") {
+    return "draw"
+  }
+  else if (gameStatus === "xNext") {
+    return (outcome === "xWins") ? "win" : "lose"
+  }
+  else if (gameStatus === "oNext") {
+    return (outcome === "oWins") ? "win" : "lose"
+  }
+  else {
+    console.error(`Error in Get Hint Color`);
+  }
+}
