b5169a565c4caf9e1df2eb6817092aa1c5cf480d
nolastemgarden <nolastemgarden@gmail.com>
2022-04-09 11:40:48 -0500

    Fix spacing

--- src/math-games/tic-tac-toe/components/panels/CoachPanel.js
+++ src/math-games/tic-tac-toe/components/panels/CoachPanel.js
@@ -1,58 +1,83 @@
 import React from 'react';
 
-
-
 // Custom Components
 import StatusHeader from './StatusHeader';
 import Commentary from "./Commentary";
 
 import HomeButton from "../buttons/HomeButton";
-import UndoMoveButton from "../buttons/UndoMoveButton";
-import ShowSolutionButton from "../buttons/ShowSolutionButton";
+// import UndoMoveButton from "../buttons/UndoMoveButton";
+// import ShowSolutionButton from "../buttons/ShowSolutionButton";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRotateLeft, faQuestion } from '@fortawesome/free-solid-svg-icons';
 
 // MUI Components
-import { Box, Container } from '@mui/material';
+import { Box, Container, Button } from '@mui/material';
 
 export default function CoachPanel(props) {
-    const { moveList, showSolution, toggleShowSolution, handleUndoClick, outcomeMap } = props
-    
+  const { moveList, showSolution, toggleShowSolution, handleUndoClick, outcomeMap } = props
+
+  const UndoMoveButton = () => {
     return (
-      <Container 
-        sx={{ 
-          width: '100%',
-          height: '45%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'space-between',
-        }}
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => handleUndoClick()}
+        disabled={moveList.length === 0}
+        sx={{ flexGrow: 2, mx: 1 }}
       >
-        <Box >
-          <StatusHeader moveList={moveList} />
-          <Commentary 
-            moveList={moveList} 
-            outcomeMap={outcomeMap}
-          />
+        <Box mr={1} display="flex" alignContent="center" >
+          <FontAwesomeIcon icon={faRotateLeft} size='lg' />
         </Box>
-        <Box display='flex' >
-          <ShowSolutionButton 
-              showSolution={showSolution}
-              toggleShowSolution={toggleShowSolution}
-          />
-          <Box id='spacer' px={1} />
-          <UndoMoveButton
-              gameOver={false}
-              moveList={moveList}
-              handleUndoClick={handleUndoClick}
-          />
-          <Box id='spacer' px={1} />
-          <HomeButton
-              gameOver={false}
-              moveList={moveList}
-              handleUndoClick={handleUndoClick}
-          />
-        </Box>
-      </Container>
+        Undo
+      </Button>
     )
+  }
+  
+  const ShowSolutionButton = () => {
+      return (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => toggleShowSolution()}
+          sx={{ flexGrow: 2, mx: 1 }}
+        >
+          <Box mr={2} display="flex" alignContent="center" >
+            <FontAwesomeIcon icon={faQuestion} size='lg' />
+          </Box>
+          {showSolution ? "Hide Solution" : "Show Solution"}
+        </Button>
+      )
+  }
+  
+
+  return (
+    <Container 
+      sx={{ 
+        width: '100%',
+        height: '45%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Box >
+        <StatusHeader moveList={moveList} />
+        <Commentary 
+          moveList={moveList} 
+          outcomeMap={outcomeMap}
+        />
+      </Box>
+      <Box mb={2} height={40} display='flex' justifyContent='space-between' >
+        <ShowSolutionButton 
+          showSolution={showSolution}
+          toggleShowSolution={toggleShowSolution}
+        />
+        <UndoMoveButton />
+        <HomeButton />
+      </Box>
+    </Container>
+  )
 }
 
 
