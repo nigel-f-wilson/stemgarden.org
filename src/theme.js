58313d0e7b833bd0d6028ccea17251aadb2371a0
nolastemgarden <nolastemgarden@gmail.com>
2022-04-16 10:59:43 -0500

    Update theme.js

--- src/theme.js
+++ src/theme.js
@@ -8,77 +8,80 @@
 import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 
 const theme = createTheme({
-    palette: {
-        white: '#FFFFFF',
-        black: '#000000',
-        darkGrey: '#212121',
-        connectFourBackground: '#b3e5fc',
-        transparent: 'rgba(0, 0, 0, 0)',
-        primary: {
-            light: '#669944',
-            main: '#2e6b12',
-            dark: '#004000',
-            contrastText: '#FFFFFF'
-        },
-        secondary: {
-            light: '#FFFFF',
-            main: '#669944',
-            dark: '#777777',
-            contrastText: '#000000'
-        },
-        text: {
-            primary: 'rgba(0, 0, 0, 0.87)',
-            secondary: 'rgba(0, 0, 0, 0.54)',
-            disabled: 'rgba(0, 0, 0, 0.38)',
-            hint: 'rgba(0, 0, 0, 0.38)',
-        },
-        action: {
-            disabledBackground: '#2e6b12',
-            disabledOpacity: '0.5'
-        },
-
+  palette: {
+    backgrounds: {
+      light: '#FFFFFF',
+      dark: '#212121',
+      connectFour: '#b3e5fc',
     },
-    components: {
-        MuiList: {
-            styleOverrides: {
-              root: {
-                // backgroundColor: 'red',
-                paddingTop: '0px',
-              },
-            },
-        },
+    white: '#FFFFFF',
+    black: '#000000',
+    darkGrey: '#212121',
+    
+    transparent: 'rgba(0, 0, 0, 0)',
+    primary: {
+      light: '#669944',
+      main: '#2e6b12',
+      dark: '#004000',
+      contrastText: '#FFFFFF'
     },
-    typography: {
-      h1: {
-        textShadow: '0.15rem 0.15rem 0.3rem black',
-        fontWeight: 200,
+    secondary: {
+      light: '#FFFFF',
+      main: '#669944',
+      dark: '#777777',
+      contrastText: '#000000'
+    },
+    text: {
+      primary: 'rgba(0, 0, 0, 0.87)',
+      secondary: 'rgba(0, 0, 0, 0.54)',
+      disabled: 'rgba(0, 0, 0, 0.38)',
+      hint: 'rgba(0, 0, 0, 0.38)',
+    },
+    action: {
+        disabledBackground: '#2e6b12',
+        disabledOpacity: '0.5'
+    },
+  },
+  components: {
+    MuiList: {
+      styleOverrides: {
+        root: {
+          paddingTop: '0px',
+        },
       },
-      pageHeader: { 
-        // color: theme.palette.common.white
-
-        textAlign: "center",
-
-        textShadow: '0.15rem 0.15rem 0.3rem black',
-        fontWeight: 200,
-      },
-      cardHeader: { 
-        textShadow: '0.15rem 0.15rem 0.3rem black',
-        fontWeight: 200,
-      },
-      button: {
-        fontSize: '1.0rem',
-        fontWeight: '700'
-      }
     },
-    status: {
+  },
+  typography: {
+    h1: {
+      textShadow: '0.15rem 0.15rem 0.3rem black',
+      fontWeight: 200,
+    },
+    pageHeader: { 
+      // color: theme.palette.common.white
 
+      textAlign: "center",
+
+      textShadow: '0.15rem 0.15rem 0.3rem black',
+      fontWeight: 200,
     },
-    shape: {
-        borderRadius: 8,
+    cardHeader: { 
+      textShadow: '0.15rem 0.15rem 0.3rem black',
+      fontWeight: 200,
     },
-    spacing: 8,
-    transitions: {    },
-    zIndex: { }
+    button: {
+      fontSize: '1.0rem',
+      fontWeight: '700'
+    }
+  },
+  status: {
+
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  spacing: 8,
+  transitions: {    },
+  zIndex: { }
 
 });
 
