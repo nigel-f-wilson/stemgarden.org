fae79e259b44bb5eca43a598267290944892b3e6
nolastemgarden <nolastemgarden@gmail.com>
2022-04-19 11:05:15 -0500

    Add TODO comment
    Regarding screen Height vs available height

--- src/hooks.js
+++ src/hooks.js
@@ -28,6 +28,10 @@ export function useHover() {
   return [ref, value];
 }
 
+
+// May want to change these hooks to use the Client Height property of the root <html> element.  
+// I think that the window.innerHeight currently used is counting height I don't actually want. 
+// document.documentElement.clientWidth & document.documentElement.clientHeight
 export function useScreenHeight() {
     const [screenHeight, setScreenHeight] = useState(window.innerHeight)
 
