190e2b6b6eb5b7c73419a956ab5e7a8f5e73f95a
nolastemgarden <nolastemgarden@gmail.com>
2022-04-20 17:57:56 -0500

    Create ColumnOfSquaresWithHoles.js

---
+++ src/math-games/connect-four/components/ColumnOfSquaresWithHoles.js
@@ -0,0 +1,46 @@
+import React, { useContext } from 'react'
+import { Box } from '@mui/material'
+
+import { ConnectFourContext } from "../ConnectFourContext";
+
+export function ColumnOfSquaresWithHoles(props) {
+  const { gameBoardConstants } = useContext(ConnectFourContext)
+  const { rowNumbers, oneSixth, oneSeventh, sixSevenths, chipSizeRelativeToSquare, } = gameBoardConstants
+
+  return (
+    <Box id="columnOfSquares"
+      position='absolute'
+      top={oneSeventh}
+      bgcolor='transparent'
+      width='100%'
+      height={sixSevenths}
+      zIndex='board'
+      display='flex'
+      flexDirection='column-reverse'
+    >
+      {rowNumbers.map(num => {
+        return (
+          <Box id="squareWithHole"
+            key={num}
+            overflow='hidden'
+            width='100%'
+            height={oneSixth}
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            boxShadow='0px 1px 1px 1px #0039cb'
+          >
+            <Box id="hole"
+              boxShadow="0 0 0 99px #0039cb"
+              borderRadius='50%'
+              bgcolor='transparent'
+              width={chipSizeRelativeToSquare}
+              height={chipSizeRelativeToSquare}
+            />
+          </Box>
+        )
+      })}
+    </Box>
+  )
+}
+
