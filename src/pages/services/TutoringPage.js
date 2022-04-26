72f5525e79f106943e60bc04aecb7b0e1a396de8
nolastemgarden <nolastemgarden@gmail.com>
2022-04-26 18:03:12 -0500

    Rename file

---
+++ src/pages/services/TutoringPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+// import { useScreenWidth } from "../hooks"
+
+// MY components
+
+// MY images
+
+// MUI Imports
+// import { Container, Box, Typography } from '@mui/material';
+// import { common } from '@mui/material/colors';
+
+
+// LandingPage is responsible for rendering a Navbar and either the Mobile or Desktop version
+// depending on screen width. The Navbar contains its own M.vs.D. rendering logic. 
+export default function TutooringPage(props) {
+  return (
+    <React.Fragment >
+     Tutoring Page
+    </React.Fragment>
+  )
+}
+
