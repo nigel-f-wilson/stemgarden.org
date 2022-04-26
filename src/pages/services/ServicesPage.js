3aa2c6ec833e257d0fb8c11b0a10fad296570eee
nolastemgarden <nolastemgarden@gmail.com>
2022-04-26 17:12:12 -0500

    Create Page stubs

---
+++ src/pages/services/ServicesPage.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+// import { useScreenWidth } from "../hooks"
+
+// MY components
+// import Navbar from "../components/navigation/Navbar";
+// import LandingCards from "../components/LandingCards";
+
+
+
+// MUI Imports
+// import { Container, Box, Typography } from '@mui/material';
+// import { common } from '@mui/material/colors';
+
+
+// LandingPage is responsible for rendering a Navbar and either the Mobile or Desktop version
+// depending on screen width. The Navbar contains its own M.vs.D. rendering logic. 
+export default function Services(props) {
+  return (
+    <React.Fragment >
+
+    </React.Fragment>
+  )
+}
