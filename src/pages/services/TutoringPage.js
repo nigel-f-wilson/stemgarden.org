638276191a36f753f83906331de48f1bb21c91ea
nolastemgarden <nolastemgarden@gmail.com>
2022-05-07 10:05:45 -0500

    Create stub from Index page

--- src/pages/services/TutoringPage.js
+++ src/pages/services/TutoringPage.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Box, Container, Typography } from '@mui/material';
 
-// import { useScreenWidth } from "../hooks"
+import { PageLayoutContext } from "../../contexts";
 
-// MY components
+// const under_construction = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1647561688/stemgarden.org/under_construction_sbiop2.png"
 
-// MY images
+// Tutoring
+// const practicing_math_facts = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1646863130/stemgarden.org/teaching/practicing_math_facts_gnhwkx.jpg"
 
-// MUI Imports
-// import { Container, Box, Typography } from '@mui/material';
-// import { common } from '@mui/material/colors';
-
-
-// LandingPage is responsible for rendering a Navbar and either the Mobile or Desktop version
-// depending on screen width. The Navbar contains its own M.vs.D. rendering logic. 
 export default function TutoringPage(props) {
   return (
-    <React.Fragment >
-     Tutoring Page
-    </React.Fragment>
+    <Background >
+      <PageHeader />
+    </Background>
   )
 }
 
+function Background(props) {
+  const { narrowScreen } = useContext(PageLayoutContext)
+  const padding = narrowScreen ? '0rem' : '2rem 1rem 3rem'
+  return (
+    <Box 
+      width='100%'
+      minHeight='100vh'
+      bgcolor="darkGrey"
+      display='flex'
+      flexDirection='column'
+      padding={padding}
+    >
+      <Container maxWidth='md'>
+        { props.children }
+      </Container>
+    </Box>
+  )
+}
+function PageHeader() {
+  return (
+    <Box pb='2rem' display='flex' flexDirection='column' >
+      <Typography align="center" variant="pageHeader" >
+        Tutoring
+      </Typography>
+      <Typography align="justify" variant="pageSubheader" px='8%' >
+        I have taught math and physics in schools and privately since 2011. 
+      </Typography>
+    </Box>
+  )
+}
