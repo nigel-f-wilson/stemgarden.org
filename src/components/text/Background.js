4f6c3deaf3b88cbaa1ef61387a9b923e77433ecd
nolastemgarden <nolastemgarden@gmail.com>
2022-05-10 20:45:55 -0500

    Create Background.js

---
+++ src/components/text/Background.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { Box, Container } from '@mui/material';
+
+import { AppContext } from "../../AppContext";
+
+export default function Background(props) {
+  const { colorTheme } = props
+
+  const { narrowScreen } = useContext(AppContext)
+  const padding = narrowScreen ? '0rem' : '2rem 1rem 3rem'
+  const bgcolor = (colorTheme === "dark") ? "darkGrey" : "white"
+  return (
+    <Box 
+      width='100%'
+      minHeight='100vh'
+      bgcolor={bgcolor}
+      display='flex'
+      flexDirection='column'
+      padding={padding}
+    >
+      <Container maxWidth='sm'>
+        { props.children }
+      </Container>
+    </Box>
+  )
+}
