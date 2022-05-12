58362763cc73d10dcab817d6697c3eb221e2ac68
nolastemgarden <nolastemgarden@gmail.com>
2022-05-12 10:33:38 -0500

    Update WhatWeGrowPage.js

--- src/pages/about/WhatWeGrowPage.js
+++ src/pages/about/WhatWeGrowPage.js
@@ -1,158 +1,159 @@
 import React from 'react'
+import { Box, Container, Typography } from '@mui/material';
 
-import { useScreenWidth } from "../../hooks"
 
-// import Footer from '../components/Footer';
-import LandingCard from "../../components/cards/LandingCard"
-import { Container, Box, Grid, Typography } from '@mui/material';
+import Background from "../../components/backgrounds/Background";
+import PageTitle from '../../components/text/PageTitle';
+import Section from "../../components/text/Section";
 
-// MY images
-const leafy_background = "https://res.cloudinary.com/nola-stem-garden/image/upload/v1647192146/stemgarden.org/leafy_background_wzclfb.jpg"
-
-// Tutoring
-const practicing_math_facts = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1646863130/stemgarden.org/teaching/practicing_math_facts_gnhwkx.jpg"
-
-// Math Games
-// const playing_the_15_game = "https://res.cloudinary.com/nola-stem-garden/image/upload/v1646685339/stemgarden.org/project_screenshots/playing_the_15_game_rcowti.jpg"
-// const playing_connect_four = "https://res.cloudinary.com/nola-stem-garden/image/upload/v1646687130/stemgarden.org/project_screenshots/playing_math_fact_connect_four_zgyzou.jpg"
-const girls_playing_connect_four = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1647052443/stemgarden.org/teaching/connect_four/girls_playing_math_fact_connect_four_j84gqo.jpg"
-
-// Chess Clubs
-const chess_at_college_track = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1647050134/stemgarden.org/chess/chess_club_college_track_st00uc.jpg"
-// const chess_at_bric = "https://res.cloudinary.com/nola-stem-garden/image/upload/v1647050028/stemgarden.org/chess/chess_club_at_bric_tvycwi.jpg"
-// const chess_demo_board = "https://res.cloudinary.com/nola-stem-garden/image/upload/v1612135192/stemgarden.org/chess/chess_demo_board_qtdzpw.jpg"
-// const simul_at_bric = "https://res.cloudinary.com/nola-stem-garden/image/upload/v1646937461/stemgarden.org/chess/simul_at_bric_nagag0.jpg"
-
-// Enrichment + Hands On
-const guitar_fret_measuring = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1646597930/stemgarden.org/teaching/hands_on/guitar_fret_spacing_jezbuk.jpg"
-const bike_gear_ratio_tutoring = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1647052266/stemgarden.org/teaching/hands_on/bike_gear_ratios_at_tutoring_s6l3ee.jpg"
-
-// What We Grow
-const banana_papaya_turmeric_flower = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1646607021/stemgarden.org/banana_papaya_turmeric_j3f7k9.jpg"
-
-const profile_with_hoe = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1647374938/stemgarden.org/nigel/profile-pic_ncluzf.jpg"
-const mushroom_background = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1647375289/stemgarden.org/garden_backgrounds/mushroom_yfjiho.jpg"
-const banana_papaya_pile = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_1000,h_1000,c_limit/v1612135416/stemgarden.org/harvests/banana_papaya_harvest_f9ml5d.jpg"
-
-const landingCards = [
-  {
-    title: "Math Tutoring",
-    linkTo: "tutoring-services",
-    imgUrl: practicing_math_facts
-
-  },
-  {
-    title: "Math Games",
-    linkTo: "math-games",
-    // imgUrl: playing_the_15_game,
-    imgUrl: girls_playing_connect_four,
-  },
-  {
-    title: "Chess Clubs",
-    linkTo: "services/tutoring",
-    imgUrl: chess_at_college_track
-  },
-  {
-    title: "STEM Enrichment",
-    linkTo: "services/tutoring",
-    imgUrl: bike_gear_ratio_tutoring,
-  },
-  {
-    title: "What We Grow",
-    linkTo: "about",
-    imgUrl: banana_papaya_turmeric_flower,
-  },
-  {
-    title: "Study Materials",
-    linkTo: "/services/tutoring",
-    imgUrl: guitar_fret_measuring,
-  },
-  {
-    title: "Our Story",
-    linkTo: "/garden/story",
-    imgUrl: mushroom_background,
-  },
-  {
-    title: "Buy Our Fruit",
-    linkTo: "/garden/for_sale",
-    imgUrl: banana_papaya_pile,
-  },
-  {
-    title: "Get Involved",
-    linkTo: "/garden/get_involved",
-    imgUrl: profile_with_hoe,
-  },
-]
-
-export default function WhatWeGrowPage() {
-  const screenWidth = useScreenWidth()
-  const smallScreen = screenWidth < 600
+export default function WhatWeGrowPage(props) {
+  
   return (
-    <React.Fragment>
-      <Background smallScreen={smallScreen} >
-        <Header />
-        <CardGrid />
-      </Background>
-      {/* <Footer smallScreen={smallScreen} /> */}
-    </React.Fragment>
-      
+    <Background >
+      <Container maxWidth='md' >
+        <PageTitle text="Our Story" />
 
-  )
-}
+        <FoundingSection />
+        <LivingSection />
+        <SolarAndRainSection />
 
-function Background(props) {
-  const gradient = (props.smallScreen) ?
-    "linear-gradient(90deg, rgba(58,94,22,0.80) 0%, rgba(58,99,22,0.80) 100%)"
-    : "linear-gradient(90deg, rgba(58,94,22,0.30) 0%, rgba(58,94,22,0.80) 10%, rgba(58,94,22,0.80) 90%, rgba(58,99,22,0.30) 100%)"
-
-  const backgroundStyles = {
-    width: '100%',
-    minHeight: '100vh',
-    height: 'auto',
-    backgroundImage: `
-      ${gradient},
-      url(${leafy_background})
-    `,
-    backgroundPosition: 'center top',
-    backgroundRepeat: 'repeat-y',
-    backgroundSize: 'cover',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    textAlign: 'center',
-  }  
-  return (
-    <Box sx={backgroundStyles} >
-      <Container maxWidth='md' id='landing' >
-        { props.children }
+        <Subjects />
+        <Pricing />
+        <Testimony />
+        <Experience />
       </Container>
-    </Box>
+    </Background>
   )
 }
 
-function Header() {
+function FoundingSection() {
+  const headerText = "The STEM Garden was founded in 2016 ..."
+  const par1 = "Shortly after completing my bachelor's in math education at Ohio University, I moved to New Orleans. I wanted to find out what all the buzz around charter schools was about but I was also excited to be able to garden year round."
+  const par2 = "I bought the first half of the garden in an online auction two months after moving to New Orleans. I had been gardening for three years at that point, working in a different place each summer. I had learned that gardening is about much more than taking care of plants, it is about taking care of soil, and that is something that you just can't do if you change location every year."
+   
   return (
-    <Box padding='4rem 0rem 2rem' >
-      <Typography align="center" variant="pageHeader" >
-          What We Grow
+    <Section 
+      headerText={headerText}
+      paragraphs={[ par1, par2 ]}
+    />
+  )
+}
+
+function LivingSection() {
+  const headerText = "Building an off-grid urban homestead"
+  const par1 = "When I bought the garden it was an overgrown trash pit. When I first began cleaning up the site I was living out of my van. Over the next five years the garden gradually became a more comfortable place to live. I built a toolshed, a screened-in kitchen, and two tiny houses on trailers. I stubbornly avoided monthly bills and dependence on any infrastructure I did not thoroughly understand. I believe in radically shortening supply chains, that, when it comes to meeting our basic needs, bringing the production as close as possible to the consumer is a good thing. It makes systems both more efficient and more resilient.   as clsoe to home  got into solar power and rain water catch systems. with one panel and battery and expanded the system as I could afford to. Similarly, I " 
+  const par2 = "manure"
+   
+  return (
+    <Section 
+      headerText={headerText}
+      paragraphs={[ par1, par2 ]}
+
+    />
+  )
+}
+
+function SolarAndRainSection() {
+  const headerText = "Solar Power and Rain Catch"
+  const par1 = "When I bought the garden it was an overgrown trash pit. When I first began cleaning up the site I was living out of my van. Over the next five years the garden gradually became a more comfortable place to live. I built a toolshed, a screened-in kitchen, and two tiny houses on trailers. I stubbornly avoided monthly bills and dependence on any infrastructure I did not thoroughly understand. I believe in radically shortening supply chains, that, when it comes to meeting our basic needs, bringing the production as close as possible to the consumer is a good thing. It makes systems both more efficient and more resilient.   as clsoe to home  got into solar power and rain water catch systems. with one panel and battery and expanded the system as I could afford to. Similarly, I " 
+  const par2 = ""
+   
+  return (
+    <Section 
+      headerText={headerText}
+      paragraphs={[ par1, par2 ]}
+
+    />
+  )
+}
+
+function Subjects() {
+  return (
+    <Box pb='2rem' display='flex' flexDirection='column' >
+      <Typography  variant="pageSubheader" >
+        Subjects
+      </Typography>
+      <Typography  variant="body1" align='justify' color='white' >
+        I teach foundational number sense, algebra, geometry, trigonometry, calculus, statistics, discrete math, and probability. Tutoring generally focuses on book-work. 
+        If you are interesten in a more hands-on experience, be sure to check out the STEM Enrichment page. 
+        {/* I also love to support homeschooling families and collectives.  */}
       </Typography>
     </Box>
   )
 }
 
-function CardGrid(props) {
+function Pricing() {
   return (
-    <Grid container padding='0 0 4rem' >
-      {
-        landingCards.map((cardData, index) => {
-          return (
-            <Grid item xs={6} sm={4} key={index} padding='0.4rem' >
-              <LandingCard data={cardData}  />
-            </Grid>
-          )
-        })
-      }
-    </Grid>
+    <Box pb='2rem' display='flex' flexDirection='column' >
+      <Typography  variant="pageSubheader" >
+        Pricing
+      </Typography>
+      <Typography  variant="body1" align='justify' color='white' gutterBottom >
+        My base reate for one-on-one tutoring is $50 per hour. For tutoring in AP and college level material I charge $65 per hour.
+        For groups of 2-8 students, pricing depends on group size and meeting location.
+      </Typography>
+      <Typography  variant="body1" align='justify' color='white' >
+        I believe that high-quality math instruction should be available to any student with the drive to learn. If affordability is an issue, 
+        don't hesitate to reach out. I can usually make it work through a combination of group discounts and work-trade agreements. I also coach
+        parents on how to best support their kids in math. This can be a more affordable option for families with kids up to high school.
+      </Typography>
+    </Box>
   )
 }
- 
+
+function Testimony() {
+  return (
+    <Box pb='2rem' display='flex' flexDirection='column' >
+      <Typography  variant="pageSubheader" >
+        Testimony
+      </Typography>
+
+      <Quote
+        quote="I wouldn't have made it through my first semester of grad school without Nigel. I had tried several other tutors before him so I can say he has a unique tallent for breaking concepts down clearly. After working with him, I could even explain the concepts to the other people in my study group. He listened to me vent about my professor, then redirected to keep me positive and on track."
+        citation="Julie Richelson, MBA Student at UNO"
+      />
+
+      <Quote
+        quote="I highly recommend Nigel without reservation! He brought much preparation and care to his teaching while encouraging his students' solving ability and thirst for knowledge with an upbeat attitude."
+        citation="William Mains, Parent of a 7th grader at WSNO"
+      />
+
+      <Quote
+        quote="While Nigel was our math teacher at the Waldorf School of New Orleans, our students were very engaged. It was the most excited I had ever seen them to go to math. The lessons he developed and taught our 4th-8th graders were so interesting and thought provoking."
+        citation="Jill Contento, Faculty Chair WSNO"
+      />
+
+
+    </Box>
+  )
+}
+
+function Quote(props) {
+  const { quote, citation } = props
+  return (
+    <Box p={1} >
+      <Typography  variant="body1" align='justify' color='white' >
+        "{quote}"
+      </Typography>
+      <Typography  variant="body1" align='right' color='white' >
+        - {citation}
+      </Typography>
+    </Box>
+  )
+}
+
+function Experience() {
+  return (
+    <Box pb='2rem' display='flex' flexDirection='column' >
+      <Typography  variant="pageSubheader" >
+        Experience
+      </Typography>
+      <Typography  variant="body1" align='justify' color='white'  >
+        I had my first teaching experiences at chess club when I was still a kid. In 2015, I earned my Bachelors degree in 
+        secondary Math Education from Ohio University after completing an internship with Step-By-Step scool in Noida, India. 
+        Since then, I have been teaching in New Orleans in schools, after school programs, and as a private tutor.  I have helped
+        my students succeed at everything from foundational number sense to college level courses in calculus, statistics, and physics.
+      </Typography>
+    </Box>
+  )
+}
