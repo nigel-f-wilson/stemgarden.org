084a8f546321e972551842d7e9e81736656cf639
nolastemgarden <nolastemgarden@gmail.com>
2022-05-12 09:03:50 -0500

    Update lineHeight with fontSize

--- src/components/text/PageTitle.js
+++ src/components/text/PageTitle.js
@@ -17,16 +17,19 @@ export default function PageTitle(props) {
         textShadow: '0.15rem 0.15rem 0.3rem black',
         fontWeight: 200,
         fontSize: '2.4rem',
+        lineHeight: '2.8rem',
         '@media (min-width: 450px)': {
           fontSize: '3.0rem',
+          lineHeight: '3.5rem',
         },
         '@media (min-width: 600px)': {
           fontSize: '4.0rem',
+          lineHeight: '4.7rem',
         },
         '@media (min-width: 900px)': {
           fontSize: '5.0rem',
+          lineHeight: '6.0rem', 
         },
-        paddingBottom: '2rem'
       }}
     >
       { text }
