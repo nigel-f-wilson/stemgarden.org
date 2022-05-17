bf8239b0aa9136d774dce8b517e660568281a3d3
nolastemgarden <nolastemgarden@gmail.com>
2022-05-17 06:18:07 -0500

    Create cloudinaryURLs.js

---
+++ src/cloudinaryURLs.js
@@ -0,0 +1,36 @@
+// What We Grow
+export const banana_papaya_turmeric_flower = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1646607021/stemgarden.org/banana_papaya_turmeric_j3f7k9.jpg"
+export const profile_with_hoe = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1647374938/stemgarden.org/nigel/profile-pic_ncluzf.jpg"
+export const mushroom_background = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1647375289/stemgarden.org/garden_backgrounds/mushroom_yfjiho.jpg"
+
+// Papaya
+export const papaya_fruit_whole = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1612135460/stemgarden.org/harvests/papaya_fruit_mirz2m.jpg'
+export const papaya_fruit_halved = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1612135462/stemgarden.org/harvests/papaya_halved_sazzwm.jpg'
+export const papaya_flower = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1646607697/stemgarden.org/flowers/papaya_flower_d3enpz.jpg'
+export const papayas_on_tree = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652720259/stemgarden.org/harvests/papaya_tree_with_green_fruit_yzzo7g.jpg'
+
+// Banana
+export const banana_hands = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652720183/stemgarden.org/harvests/banana_hands_vhxa6v.jpg'
+export const banana = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1612190844/nolastemgarden/banana/banana_bunch_iusnwi.jpg'
+export const banana_flower = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1646607722/stemgarden.org/flowers/banana_flower_beecdn.jpg'
+export const banana_papaya_pile = "https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1612135210/stemgarden.org/harvests/banana_papaya_top_zzqtos.jpg"
+
+// Fig 
+export const fig = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1646606908/stemgarden.org/harvests/fig_harvest_rcmtsu.jpg'
+export const fig_2 = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1612190789/nolastemgarden/fig/fig_harvest_qmwyxm.jpg'
+export const fig_missing_bite = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652720191/stemgarden.org/harvests/fig_missing_bite_laugkv.jpg'
+export const three_figs = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652720224/stemgarden.org/harvests/three_figs_unwxfd.jpg'
+
+// Loquat
+export const loquat_bunch = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652702918/stemgarden.org/harvests/loquat_harvest_lfyeee.jpg'
+export const loquat_pile = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652703028/stemgarden.org/harvests/loquat_pile_sryz1n.jpg'
+export const loquats_on_tree = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652720249/stemgarden.org/harvests/loquats_on_tree_gezrfc.jpg'
+
+// Turmeric
+export const turmeric_harvest = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1612135511/stemgarden.org/harvests/turmeric_harvest_iwfpqu.jpg'
+export const turmeric_flower = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1612191032/stemgarden.org/flowers/turmeric_flower_fwvwmj.jpg'
+
+// Other Harvests
+export const tomato_cuke_harvest = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652701628/stemgarden.org/harvests/tomato_cuke_harvest_h3ymvj.jpg'
+export const cucumber_pile = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652730445/stemgarden.org/harvests/cucumber_pile_fd91a2.jpg'
+export const tobacco_plant = 'https://res.cloudinary.com/nola-stem-garden/image/upload/w_500,h_500,c_limit/v1652730424/stemgarden.org/harvests/tobacco_plant_yiy4r6.jpg'
