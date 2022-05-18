ebf92cda15cde673a73b2857bf2b47078687133b
nolastemgarden <nolastemgarden@gmail.com>
2022-05-18 15:25:40 -0500

    Remove padding from background

--- src/components/backgrounds/Background.js
+++ src/components/backgrounds/Background.js
@@ -6,7 +6,6 @@ import { AppContext } from "../../AppContext";
 export default function Background(props) {
   const { colorTheme, narrowScreen } = useContext(AppContext)
   
-  const padding = narrowScreen ? '0rem' : '2rem 1rem 3rem'
   const bgcolor = (colorTheme === "dark") ? "darkGrey" : "white"
   
   return (
@@ -16,7 +15,6 @@ export default function Background(props) {
       bgcolor={bgcolor}
       display='flex'
       flexDirection='column'
-      padding={padding}
     >
       { props.children }
     </Box>
