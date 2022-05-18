a5f790070a3846a0e27337f9a35f3e75a916d92a
nolastemgarden <nolastemgarden@gmail.com>
2022-05-18 15:19:20 -0500

    Add PageHeader export to /index

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -1,3 +1,4 @@
+export {default as PageHeader} from './PageHeader';
 export {default as PageTitle} from './PageTitle';
 export {default as PageSubtitle} from './PageSubtitle';
 export {default as Section} from './Section';
