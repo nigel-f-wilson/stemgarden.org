737ec28361af5e72b0cde12a6b4051f9754749c3
nolastemgarden <nolastemgarden@gmail.com>
2022-05-21 12:58:16 -0500

    Update PuzzlesIndexPage.js

--- src/pages/resources/PuzzlesIndexPage.js
+++ src/pages/resources/PuzzlesIndexPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Link as RouterLink } from 'react-router-dom';
-import { Container, Grid, Card, CardMedia, Typography, Button, Stack } from '@mui/material';
+import { Box, Container, Grid, Card, Typography, Button, Stack } from '@mui/material';
 
 import { Background } from "../../components/backgrounds";
 import { PageHeader } from "../../components/text";
@@ -8,22 +7,62 @@ import { PageHeader } from "../../components/text";
 import { puzzleData } from "./puzzleData"
 
 export default function PuzzlesIndexPage() {
+
+  // const [selectedTags, setSelectedTags] = useState({
+  //   "pythagorean theorem": true,
+  //   "classic puzzles": true,
+  //   "factoring": true,
+  //   circles: true,
+  //   area: true,
+
+  //   // combinations
+  //   // logic
+  //   // fractions
+  //   // sequences
+  //   // permutations
+  //   // 'tree graph'
+  //   // percents
+  //   // probability
+  //   // circles
+  //   // area
+  //   // distance
+  //   // nets
+  //   // "pythagorean theorem"
+  //   // "algebra"
+  //   // "classic puzzles"
+  //   // "factoring"
+  //   // "perfect squares"
+  //   // quadratic
+  //   // "linear system"
+  //   // "unit rates"
+  //   // triangle numbers
+  //   // "arithmetic series"
+  //   // multiplication
+  //   // averages
+  // }) 
   
   return (
     <Background>
-      <Container maxWidth="md" >
+      <Container maxWidth="md" sx={{ pb: '3rem' }} >
         <PageHeader 
           title="Puzzles"
-          subtitle="Each can be used with students at a wide range of levels. It's not about the answer, it about how you explore and explain."
-          // subtitle="I assembled this colletion of math puzzles over many years, pulling from many sources. Introduce a puzzle, allow space for struggle and confusion. Where other sheets are great for practicing basic skills these puzzles are for practicing high level problem solving. The important thing when using these puzzles is not the final answer, it is about how you approach the problem. Practice organizing your thoughts, breaking problems down into simpler parts, creating your own simpler problems to help as you explore the the puzzles in their original form. How kids get answers to these puzzles will vary a lot depending on what techniques the have recieve instruction on. None of these puzzles is meant to be solved one way, to be part of this collection a puzzle had to bear a connection to a high school standard while still being accessible to much younger kids with no formal teaching on that standard. Use the same puzzle more tSome of these are old classics and some These are my personal favorites ollection consists of my personal favorites of one page puzzles I have assembeled from many sources."
+          // subtitle="My personal favorites, collected over many years of happy nerding."
+          // subtitle="I assembled this colletion of math puzzles over many years, pulling from many sources. Introduce a puzzle, allow space for struggle and confusion. Where other sheets are great for practicing basic skills these puzzles are for practicing high level problem solving. The important thing when using these puzzles is not the final answer, it is about how you approach the problem. Try out different way of organizing your thoughts on paper. Break problems down into simpler parts and create your own simpler warm-up problems to help as you get started in your exploration. How kids get answers to these puzzles will vary a lot depending on what techniques the have recieved previous instruction on. None of these puzzles is meant to be solved just one way. To be part of this collection a puzzle had to bear a connection to a high school standard while still being accessible to much younger kids with no formal teaching on that standard. Avoid any hint giving as much as possible. If you are an adult and are struggling to solve one of these, great! share that sturggle with a youngster! It is vital for kids to see adults try at things where success is not a foregone conclusion. Don't just teach them what you know; Teach them what you do when you don't already know. It is ok if a kid isn't able to solve one of these puzzles the day it is first assigned and has to revisit it weeks later. An excellent way to use these puzzles is to present the same one repeatedly to a student at an interval of a few years. Let them observe how their approach changes as thier mathematical experience grows."
         />
         <Grid container spacing={2} >
+          {/* <Grid item xs={12} >
+            <Filter 
+              selectedTags={selectedTags} 
+              setSelectedTags={setSelectedTags}
+            />
+          </Grid> */}
           {
-            puzzleData.map(cardData => {
-              const { title, tags, imgUrl, googleDriveCopyUrl } = cardData 
+            puzzleData.filter(cardData => {
+              return true
+            }).map(cardData => {
               return (
-                <Grid item xs={6} md={4} key={index} >
-                  <MathGameCard data={cardData}  />
+                <Grid item xs={6} sm={4} md={3} key={cardData.title} >
+                  <PuzzleCard cardData={cardData}  />
                 </Grid>
               )
             })
@@ -34,32 +73,48 @@ export default function PuzzlesIndexPage() {
   )
 }
 
-function MathGameCard(props) {
-  const { title, linkTo, imgUrl, disabled } = props.data
+// function Filter(props) {
+//   const { selectedTags, setSelectedTags } = props
+//   return (
+//     <Card raised sx={{ height: '100%', borderRadius: 1, bgcolor: 'white', padding: 2 }} >
+//       <Typography variant='h4' children="Filter by prerequisite" />
+//       {/* <Typography children="While these puzzles are great for fostering creative problem solving, I don't reccomend giving a puzzle that requires the pythagorean theorem to a student who has never been icreative Some of these can be solved with pure creativity but other" /> */}
+//       <Typography variant='h4' children="Filter by source" />
 
+      
+//     </Card>
+//   )
+// }
+
+function PuzzleCard(props) {
+  const { title, imgUrl, googleDriveCopyUrl } = props.cardData 
+  const imageStyles = {
+    width: '100%',
+    paddingTop: '100%',
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'contain',
+  }  
   return (
     <Card raised
       sx={{ height: '100%', borderRadius: 1, 
-        bgcolor: 'black'
+        bgcolor: 'white'
       }}
     >
       <Stack height='100%' display='flex' justifyContent='space-between' p={1} >
-        <Typography 
-          children={title}
-          color="white"
-          align='center'
-          fontSize='1.2rem'
-        />
-        <CardMedia
-          component="img"
-          image={imgUrl}
-          alt={title}
-        />
+        <Box display='flex' justifyContent='center' alignItems='center' minHeight="4.3rem" >
+          <Typography 
+            children={title}
+            color="black"
+            align='center'
+            fontSize='1.5rem'
+            lineHeight='1.9rem'
+          />
+        </Box>
+        <Box sx={{ backgroundImage: `url(${imgUrl})`, ...imageStyles}} />
         <Button 
-          component={RouterLink} 
-          to={linkTo}
-          disabled={disabled}
-          children="Play Now!"
+          children="Make a Copy"
+          href={googleDriveCopyUrl}
           variant='contained'
           sx={{ mt: 1 }}
         />
