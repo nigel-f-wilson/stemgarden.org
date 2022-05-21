f226924d4e9fa891702df040e2844848328b67c0
nolastemgarden <nolastemgarden@gmail.com>
2022-05-21 18:10:06 -0500

    Move comments to bottom

--- src/pages/services/ChessClubsPage.js
+++ src/pages/services/ChessClubsPage.js
@@ -28,46 +28,11 @@ export default function ChessClubsPage(props) {
         <ChessTeachingMaterialsSection />
         <ChessClubHostAppSection />
 
-        {/* <TestimonySection /> */}
-        {/* <ExperienceSection /> */}
       </Container>
     </Background>
   )
 }
 
-// Private and Group Lessons
-// 
-// Chess Club in the Garden
-// *** All in-person clubs are suspended until further notice due to Covid-19 ***
-
-// When safe, chess in the garden will be offered every Sunday afternoon from 2-4pm I will be launching a Sunday afternoon chess club in the STEM Garden as soon as it is safe to do so.
-
-// Afterschool Clubs
-// *** All in-person clubs are suspended until further notice due to Covid-19 ***
-// I currently host an afterschool chess club in partnership with the Waldorf School of New Orleans. I am currently seeking schools to partner with to establish new afterschool chess clubs. Need help establishing a chess club at your school? Call me!
-
-// Why Learn Chess?
-// Learning chess helps kids experience firsthand that thinking hard and having fun can go together.
-// Math and Chess have a lot in common. They both require creative, yet disiplined, thinking. Math is very useful, and so kids are required to learn it in school. Unfortunately, that pressure to learn math often causes them to lose sight of the fact that doing math can be really fun. When students feel under pressure in math classes they start asking "Why do I have to learn this?" and "When am I going to need this?"
-
-// By contrast, Chess is an abstract game with no practical applications. No one is required to learn it, and yet, something about it makes kids feel drawn to apply their full reasoning abilities. No one has ever inturrupted a chess lesson to ask me "When am I going to need this?" Chess makes kids want to think hard. It is an excellent medium for teaching kids to think out loud and to think through questioning, both skills which carry over and make them more effective learners in all other subjects. including math. Players also develop greater self-awareness about how their emotions affect their thinking process
-
-// Pricing
-// •One-on-one lessons are offered for $35 per hour. •Lessons for 2 students are offered for $40 per hour. •Lessons for groups of 3 to 6 students are $15 per student per hour. •Lessons for groups of 7 to 12 students are $10 per student per hour. •Sunday Chess Club in the garden is offered with a suggested donation of $5-10. •The cost for facilitating afterschool clubs can be negotiated on a case-by-case basis.
-// As with my math tutoring services, I want chess lessons to be accessible to any student who is really motivated to apply themselves. If cost is an issue please reach out to me personally, I am happy to barter or make work-trade agreements, lessons in exchange for help with garden projects.
-
-// Chess Teaching Experience
-// I have played chess for 18 years and I am currently rated ~1600, or in the 90th percentile of players on chess.com. I'm a long way from the title of 'Master' but I have a knack for explaining the game. At the club I went to as a kid I was encouraged to play 'teaching games' with other kids as soon as I had something to teach, so I've been teaching chess even longer than I've been teaching math!
-// Besides establishing chess clubs at College Track and the Waldorf School I have taught chess summer camps offered by Strategic Thoughts in partnership with NORD.
-
-// Chess & Test Prep
-// I got serious about running youth chess clubs while working at College Track. CT placed a strong emphasis on preping kids for fast-paced high-stakes standardized tests. I reflected on my own experience getting a scholarship based on my ACT score and I realized that, though the math topics covered on the ACT had been gone over in school, what had really prepared me for the test's pressure level and pace was all the speed chess I had played as a kid. It was chess that taught me to cope with time pressure, to focus my mind so that I could think clearly in a stressful circumstance. I offer discounted rates on SAT and ACT prep for students who add on chess lessons.
-
-
-
-
-
-function Offering() {
 function WhyChessSection() {
   const headerText = "Why I Teach Chess"
   const par1 = "My very first teaching experiences were at chess club, years before I decided to become a math teacher. Chess Club is more than just good cheap entertainment, it is a medium through which kids learn life lessons on sportsmanship, psychology, and strategy. Chess helps kids remember that thinking really hard being playful are not mutually exclusive."
@@ -166,3 +131,33 @@ const chessTeachingMaterials = [
     googleDriveCopyUrl: "/copy",
   },
 ]
+
+
+// Private and Group Lessons
+// 
+// Chess Club in the Garden
+// *** All in-person clubs are suspended until further notice due to Covid-19 ***
+
+// When safe, chess in the garden will be offered every Sunday afternoon from 2-4pm I will be launching a Sunday afternoon chess club in the STEM Garden as soon as it is safe to do so.
+
+// Afterschool Clubs
+// *** All in-person clubs are suspended until further notice due to Covid-19 ***
+// I currently host an afterschool chess club in partnership with the Waldorf School of New Orleans. I am currently seeking schools to partner with to establish new afterschool chess clubs. Need help establishing a chess club at your school? Call me!
+
+// Why Learn Chess?
+// Learning chess helps kids experience firsthand that thinking hard and having fun can go together.
+// Math and Chess have a lot in common. They both require creative, yet disiplined, thinking. Math is very useful, and so kids are required to learn it in school. Unfortunately, that pressure to learn math often causes them to lose sight of the fact that doing math can be really fun. When students feel under pressure in math classes they start asking "Why do I have to learn this?" and "When am I going to need this?"
+
+// By contrast, Chess is an abstract game with no practical applications. No one is required to learn it, and yet, something about it makes kids feel drawn to apply their full reasoning abilities. No one has ever inturrupted a chess lesson to ask me "When am I going to need this?" Chess makes kids want to think hard. It is an excellent medium for teaching kids to think out loud and to think through questioning, both skills which carry over and make them more effective learners in all other subjects. including math. Players also develop greater self-awareness about how their emotions affect their thinking process
+
+// Pricing
+// •One-on-one lessons are offered for $35 per hour. •Lessons for 2 students are offered for $40 per hour. •Lessons for groups of 3 to 6 students are $15 per student per hour. •Lessons for groups of 7 to 12 students are $10 per student per hour. •Sunday Chess Club in the garden is offered with a suggested donation of $5-10. •The cost for facilitating afterschool clubs can be negotiated on a case-by-case basis.
+// As with my math tutoring services, I want chess lessons to be accessible to any student who is really motivated to apply themselves. If cost is an issue please reach out to me personally, I am happy to barter or make work-trade agreements, lessons in exchange for help with garden projects.
+
+// Chess Teaching Experience
+// I have played chess for 18 years and I am currently rated ~1600, or in the 90th percentile of players on chess.com. I'm a long way from the title of 'Master' but I have a knack for explaining the game. At the club I went to as a kid I was encouraged to play 'teaching games' with other kids as soon as I had something to teach, so I've been teaching chess even longer than I've been teaching math!
+// Besides establishing chess clubs at College Track and the Waldorf School I have taught chess summer camps offered by Strategic Thoughts in partnership with NORD.
+
+// Chess & Test Prep
+// I got serious about running youth chess clubs while working at College Track. CT placed a strong emphasis on preping kids for fast-paced high-stakes standardized tests. I reflected on my own experience getting a scholarship based on my ACT score and I realized that, though the math topics covered on the ACT had been gone over in school, what had really prepared me for the test's pressure level and pace was all the speed chess I had played as a kid. It was chess that taught me to cope with time pressure, to focus my mind so that I could think clearly in a stressful circumstance. I offer discounted rates on SAT and ACT prep for students who add on chess lessons.
+
