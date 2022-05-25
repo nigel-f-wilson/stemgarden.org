dc0ca55e058069654c7e8877bdd7732ebf8fdb00
nolastemgarden <nolastemgarden@gmail.com>
2022-05-25 15:25:31 -0500

    add new component files to indexed export

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -1,5 +1,8 @@
+export {default as Paragraph} from './Paragraph';
 export {default as PageHeader} from './PageHeader';
 export {default as PageTitle} from './PageTitle';
 export {default as PageSubtitle} from './PageSubtitle';
 export {default as Section} from './Section';
 export {default as Quote} from './Quote';
+export {default as PictureRow} from './PictureRow';
+
