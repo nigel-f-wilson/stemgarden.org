1ef2650d9a99ba24e744aee54f8d99f16b810e07
nolastemgarden <nolastemgarden@gmail.com>
2022-06-17 17:38:08 -0500

    Make Status Header work in bot playModes

--- src/pages/resources/math-games/magic-square-games/fifteen-game/components/panels/StatusHeader.js
+++ src/pages/resources/math-games/magic-square-games/fifteen-game/components/panels/StatusHeader.js
@@ -1,36 +1,64 @@
 import React from 'react';
+import { Typography } from '@mui/material';
 
 import { status } from "../../../magicSquareHelpers";
 
-// MUI Components
-import { Typography } from '@mui/material';
-
 export default function StatusHeader(props) {
-  const { moveList, } = props
+  const { moveList, playMode, humanGoesFirst } = props
+  console.assert((playMode === "playVsBot" || playMode === "playVsFriend"), `StatusHeader recieved invalid playMode prop: ${playMode}`)
   
+  let message = "Error in switch."
   const gameStatus = status(moveList);
 
-  function statusMessage(gameStatus) {
+  if (playMode === "playVsFriend") {
     switch (gameStatus) {
-      case "xWins":
-        return "Player One Wins!"
-      case "oWins":
-        return "Player Two Wins!"
-      case "draw":
-        return "Game Over! Draw."
-      case "xNext":
-        return "Player One's turn."
-      case "oNext":
-        return "Player Two's turn."
-      default:
-        return "Error";
+    case "xWins":
+      message = "Player One Wins!"
+      break;
+    case "oWins":
+      message = "Player Two Wins!"
+      break;
+    case "draw":
+      message = "Game Over! Draw."
+      break;
+    case "xNext":
+      message = "Player One's turn."
+      break;
+    case "oNext":
+      message = "Player Two's turn."
+      break;
     }
   }
-
+  else {
+    switch (gameStatus) {
+    case "xWins":
+      message = (humanGoesFirst) ? "Game Over. Human Wins!" : "Game Over. Bot Wins!"
+      break;
+    case "oWins":
+      message = (humanGoesFirst) ? "Game Over. Bot Wins!" : "Game Over. Human Wins!" 
+      break;
+    case "draw":
+      message = "Game Over! Draw."
+      break;
+    case "xNext":
+      message = (humanGoesFirst) ? "Human's Turn" : "Bot's Turn" 
+      break;
+    case "oNext":
+      message = (humanGoesFirst) ? "Bot's Turn" : "Human's Turn"
+      break;
+    }
+  }
+    
   return (
-    <Typography pt={2} color="common.white" align='center' component='h1' variant='h4' noWrap gutterBottom>
-      {statusMessage(gameStatus)}
-    </Typography>
+    <Typography 
+      children={message}
+      color="common.white" 
+      align='center' 
+      component='h1' 
+      variant='h4' 
+      pt={2}
+      gutterBottom
+      noWrap 
+    />
   )
 }
-
