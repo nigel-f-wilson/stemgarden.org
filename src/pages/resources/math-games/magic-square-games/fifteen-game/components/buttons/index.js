2ddc498631e39cf4b722df7b0f43e88d3295e7e9
nolastemgarden <nolastemgarden@gmail.com>
2022-06-24 12:57:47 -0500

    Create index of buttons

---
+++ src/pages/resources/math-games/magic-square-games/fifteen-game/components/buttons/index.js
@@ -0,0 +1,7 @@
+export {default as UndoMoveButton} from './UndoMoveButton';
+export {default as NewGameButton} from './NewGameButton';
+export {default as HomeButton} from './HomeButton';
+export {default as BotGoFirstButton} from './BotGoFirstButton';
+export {default as DifficultyModeButton} from './DifficultyModeButton';
+
+
