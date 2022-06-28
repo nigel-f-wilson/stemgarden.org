2771e8fc6e236f9119720843120445067c81f8b0
nolastemgarden <nolastemgarden@gmail.com>
2022-06-28 14:11:15 -0500

    Update TopLandingSection.js

--- src/pages/landing/TopLandingSection.js
+++ src/pages/landing/TopLandingSection.js
@@ -17,56 +17,60 @@ import {
 } from "../../cloudinaryURLs";
 
 export default function TopLandingSection(props) {
-  const { availableHeight, availableWidth } = useContext(AppContext)
-  // Cards are fixed aspect ratio 25vh:30vh so to decide if we render 2 or 3 cards we need to know if the width of the box is sufficient for 3 x 25vh.
-  const hideThirdCard = 0.75 * availableHeight > availableWidth
-
-  // const ref = useREf()
+  const { availableHeight, screenWidth } = useContext(AppContext)
+  const availableWidth = (screenWidth > 900) ?  900 : screenWidth 
+  const hideThirdCard = screenWidth < 500
+  console.log(`hideThirdCard: ${hideThirdCard}`);
+  const sectionHeight = (availableHeight > availableWidth) ? "100vh" : `${availableWidth}px`
+  
+  const cardRowHeight = (availableWidth === 900) ? "340px" 
+    : hideThirdCard ? `${Math.floor(availableWidth * 1.2 / 2)}px` 
+    : `${Math.floor(availableWidth * 1.2 / 3)}px`
+  const cardWidth = (availableWidth === 900) ? "280px" 
+    : hideThirdCard ? `${Math.floor(availableWidth / 2)}px` 
+    : `${Math.floor(availableWidth / 3)}px`
 
   return (
-    <BackgroundSection bg="leafy" id="top"  >
+    <BackgroundSection bg="leafy" id="top" sectionHeight={sectionHeight}  >
       <Box id="title" 
         display='flex'
         flexDirection='column'
-        justifyContent="center"
-        flex="2 0 30vh" 
+        paddingY={{ xs: '2.0rem', sm: '3.0rem' }} 
       >
         <PageTitle text="Sowing seeds of" />
         <PageTitle text="life-long learning" />
-        <PageTitle text="and DIY-spirit" />
-      </Box>
-      <Box id="subtitle"
-        display='flex'
-        alignItems="center"
-        flex="2 0 20vh" 
-        paddingX={{ xs: 0, sm: '2.5rem'}}
-      >
-        <PageSubtitle text="The STEM Garden is a micro-farm in the heart of New Orleans. We sell organic banana, papaya, fig, turmeric, and more. We also offer tutoring services and free resources for learning math." align="justify" />
+        <PageTitle text="and DIY-spirit" gutterBottom />
+        <PageSubtitle text="The STEM Garden is a micro-farm in the heart of New Orleans. We sell organic banana, papaya, fig, turmeric, and more. We also offer tutoring services and free resources for learning math." align="justify" gutterBottom />
+
       </Box>
       <Box 
-        id='landing-cards'
+        id='landing-card-row'
+        height={cardRowHeight}
         display='flex' 
-        flex="2 0 35vh" 
         justifyContent='space-evenly'
-        alignItems="center"
+        alignItems="stretch"
       > 
         <HashLink to="#what-we-grow" smooth  > {/*  ref={ref} */}
           <LandingCard 
             title="What We Grow" 
             imgUrl={banana_papaya_turmeric_flower} 
+            cardWidth={cardWidth}
           />
         </HashLink>
         <RouterLink to="services"  >
           <LandingCard 
             title="Teaching Services" 
+            shortTitle="Services"
             imgUrl={practicing_math_facts} 
+            cardWidth={cardWidth}
           />
         </RouterLink>
         <Box display={ hideThirdCard ? 'none' : 'block' } > 
-          <RouterLink to="math-games"  >
+          <RouterLink to="resources/math-games"  >
             <LandingCard 
               title="Math Games" 
               imgUrl={girls_playing_connect_four} 
+              cardWidth={cardWidth}
             />
           </RouterLink>
         </Box>
@@ -76,43 +80,45 @@ export default function TopLandingSection(props) {
 }
 
 function LandingCard(props) {
-  const { title, imgUrl } = props
-
+  const { title, shortTitle, imgUrl, cardWidth } = props
+  // const cardTitleHeight = `${Math.floor(cardWidth / 5)}px`
   return (
-    <Card raised 
-      sx={{ 
-        bgcolor: 'darkGrey',
-        position: 'relative',
-        margin: 1, 
-        paddingTop: '30vh',
-        borderRadius: "1rem",
-        width: "25vh"
-      }}
-    >
-      <CardMedia
-        component="img"
-        image={imgUrl}
-        alt={title}
+    <Box width={cardWidth} height="100%" padding={1} >
+      <Card raised 
         sx={{ 
-          position: 'absolute', 
-          top: 0,
-          zIndex: 10
+          height: '100%',
+          width: "100%",
+          bgcolor: 'darkGrey',
+          position: 'relative',
+          borderRadius: "1rem",
         }}
-      />
-      <Box 
-        position='absolute' 
-        bottom={0}
-        zIndex={20}
-        width="100%"
-        height="6vh"
-        padding="7% 0 2%"
-        sx={{ background: "linear-gradient(0deg, rgba(32,32,32,1) 85%, rgba(32,32,32,0.5) 94%, rgba(32,32,32,0.1) 100%)" }}
-        textDecoration='none'
-
       >
-        <CardTitle text={title}  />
-        {/* <FontAwesomeIcon icon={faArrowCircleDown} color="white" iconSize="2x" /> */}
-      </Box>
-    </Card>
+        <CardMedia
+          component="img"
+          image={imgUrl}
+          alt={title}
+          sx={{ 
+            position: 'absolute', 
+            top: 0,
+            zIndex: 10
+          }}
+        />
+        <Box 
+          position='absolute' 
+          bottom={0}
+          zIndex={20}
+          width="100%"
+          height='23%'
+          // height={cardTitleHeight}
+          textDecoration='none'
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          sx={{ background: "linear-gradient(0deg, rgba(32,32,32,1) 85%, rgba(32,32,32,0.5) 94%, rgba(32,32,32,0.1) 100%)" }}
+        >
+          <CardTitle text={title}  />
+        </Box>
+      </Card>
+    </Box>
   )
 }
