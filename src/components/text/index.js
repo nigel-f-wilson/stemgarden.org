ff076b20a564b587757ce2a56328561f4a271fed
nolastemgarden <nolastemgarden@gmail.com>
2022-06-28 07:20:55 -0500

    Add new file exports to index

--- src/components/text/index.js
+++ src/components/text/index.js
@@ -2,6 +2,10 @@ export {default as Paragraph} from './Paragraph';
 export {default as PageHeader} from './PageHeader';
 export {default as PageTitle} from './PageTitle';
 export {default as PageSubtitle} from './PageSubtitle';
+export {default as CardTitle} from './CardTitle';
+export {default as ButtonLabel} from './ButtonLabel';
+
+
 export {default as Section} from './Section';
 export {default as Quote} from './Quote';
 export {default as PictureRow} from './PictureRow';
