c80bfd1eaad0370ca176084ee2a0c53ac98f44cf
nolastemgarden <nolastemgarden@gmail.com>
2022-06-28 07:21:28 -0500

    Rely on context to offset header by nav height

--- src/components/text/PageHeader.js
+++ src/components/text/PageHeader.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Box } from '@mui/material';
 
 import { PageTitle, PageSubtitle } from "./";
+import { AppContext } from "../../AppContext";
 
 export default function PageHeader(props) {
   const { title, subtitle } = props
+  const { navbarHeightPx } = useContext(AppContext)
+  const marginTop = `${navbarHeightPx}px`
 
   return (
-    <Box p='3.5rem 10%' >
+    <Box 
+      marginTop={marginTop}
+      paddingY={{ xs: '2rem', sm: '3.5rem' }} 
+      // paddingX="10%" 
+    >
       <PageTitle text={title} gutterBottom={subtitle} />
       <PageSubtitle text={subtitle}  />
     </Box>
   )
 }
-
-
-
-
