a7f00e27908944b90a86aad2d92cc48f2f02cb9d
nolastemgarden <nolastemgarden@gmail.com>
2022-06-28 14:07:09 -0500

    Add file exports to navigation index

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,6 +1,9 @@
 export { default as Navbar } from "./Navbar";
+export { default as Footer } from "./Footer";
 export { default as Logo } from "./Logo";
 export { default as DesktopNavbar } from "./DesktopNavbar";
 export { default as MobileNavbar } from "./MobileNavbar";
 export { default as MobileMenu } from "./MobileMenuDrawer";
+export { default as ScrollToTopButton } from "./ScrollToTopButton";
+
 
